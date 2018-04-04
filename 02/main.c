5f5d62525adff5f1678d5c0ebe4de846e666269d
Teruteru <teruterubouzu1024@gmail.com>
2018-04-05 00:23:32 +0900

    Fix typo

--- 02/main.c
+++ 02/main.c
@@ -19,28 +19,28 @@ int main(int argc, char **argv)
     char *in1 = "MEsDAgcAAgEgAiAoQPNcS7L4k+q2qf3U7uyujtwRQNS3pLKN/zrRGERGagIgFjdV1JlqHF8BiIQne0/E3jVM7hWda/USrFI58per45s=";
     char in2[IN2_SIZE];
     uint64_t in1Length = strlen(in1);
-    uint64_t verifire;
-    size_t verifireLength;
+    uint64_t verifier;
+    size_t verifierLength;
 
     gettimeofday(&tv, NULL);
     srand48(((tv.tv_usec & 0xFFFFFFFF) << 32) ^ ((tv.tv_usec >> 32) & 0xFFFFFFFF) ^ tv.tv_sec);
-    verifire = ((uint64_t)mrand48() << 33) ^ (((uint64_t)mrand48()<<16) & 0xffffffffULL) ^ (((uint64_t)mrand48()) & 0xffffffffULL);
+    verifier = ((uint64_t)mrand48() << 33) ^ (((uint64_t)mrand48() << 16) & 0xffffffffULL) ^ (((uint64_t)mrand48()) & 0xffffffffULL);
     c = malloc(sizeof(SHA_CTX));
     if (c == NULL)
     {
         exit(EXIT_FAILURE);
     }
     memset(c, 0, sizeof(SHA_CTX));
-    for (;; verifire++)
+    for (;; verifier++)
     {
         SHA1_Init(c);
         SHA1_Update(c, in1, in1Length);
-        verifireLength = snprintUInt64(in2, IN2_SIZE, verifire);
-        SHA_Update(c, in2, verifireLength);
+        verifierLength = snprintUInt64(in2, IN2_SIZE, verifier);
+        SHA_Update(c, in2, verifierLength);
         SHA1_Final(md, c);
         if (md[0] == 0 && md[1] == 0 && md[2] == 0 && md[3] == 0 && md[4] == 0)
         {
-            printf("verifire : %" PRIu64 "\n", verifire);
+            printf("verifier : %" PRIu64 "\n", verifier);
             for (i = 0; i < SHA_DIGEST_LENGTH; i++)
             {
                 printf("%02x", md[i]);
