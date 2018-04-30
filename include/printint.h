de50e523caee849b68afe72c758e439ba166e956
Teruteru <teruterubouzu1024@gmail.com>
2018-04-30 09:55:02 +0900

    Update header

--- include/printint.h
+++ include/printint.h
@@ -10,8 +10,8 @@ extern "C" {
 
 #include <stdint.h>
 
-extern size_t snprintUInt64(char * restrict, size_t, uint64_t);
-extern size_t snprintInt64(char * restrict, size_t, int64_t);
+size_t snprintUInt64(char * restrict, size_t, uint64_t);
+size_t snprintInt64(char * restrict, size_t, int64_t);
 
 #ifdef __cplusplus
 }
