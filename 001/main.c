d43d59dd66dea5b03a8c661832ea401d9e2cbd6a
Teruteru <teruterubouzu1024@gmail.com>
2018-08-26 16:13:08 +0900

    Install gettext

--- 001/main.c
+++ 001/main.c
@@ -1,8 +1,12 @@
 
 #include "main.h"
+#define _(string) gettext(string)
 
 int main(int argc, char** argv){
-	printf("Hello world!\n");
+	setlocale (LC_ALL, "");
+	bindtextdomain (PACKAGE, LOCALEDIR);
+	textdomain (PACKAGE);
+	printf(_("Hello world!\n"));
 	return 0;
 }
 
--- 001/main.h
+++ 001/main.h
@@ -1,7 +1,11 @@
 
 #ifndef MAIN_H
 #define MAIN_H
+#include "config.h"
+#include "gettext.h"
 #include <stdio.h>
+#define PACKAGE "study"
+#define LOCALDIR NULL
 
 #endif
 
