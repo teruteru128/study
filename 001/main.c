3cf38f7a1909ba7a67357b9898ae689105bfc35e
Teruteru <teruterubouzu1024@gmail.com>
2018-09-22 21:48:41 +0900

    Add debug code
    Add cppflag to Makefile.am

--- 001/main.c
+++ 001/main.c
@@ -4,6 +4,7 @@
 int main(int argc, char** argv){
 	setlocale (LC_ALL, "");
 	bindtextdomain (PACKAGE, LOCALEDIR);
+	puts(LOCALEDIR);
 	textdomain (PACKAGE);
 	printf(_("Hello world!\n"));
 	return 0;
