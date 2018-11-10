1f65d13ee0bc01f7fc064c8fe866c61aac2bf452
Teruteru <teruterubouzu1024@gmail.com>
2018-11-10 15:01:16 +0900

    Update rsa calculator

--- 031/calc.c
+++ 031/calc.c
@@ -27,9 +27,8 @@ int main(int argc, char* argv[])
 
 	size_t i;
 	mpz_t num[2];
-	mpz_t multply;
 
-	mpz_inits(num[0], num[1], multply);
+	mpz_inits(num[0], num[1], NULL);
 
 	fp = fopen(fname, "r");
 	if(fp == NULL){
@@ -45,9 +44,81 @@ int main(int argc, char* argv[])
 		}
 		mpz_set_str(num[i], f, 16);
 	}
-	mpz_mul(multply, num[0], num[1]);
-	printf("%ldbits\n", mpz_sizeinbase(multply, 2));
 	fclose(fp);
 	free(buf);
+	
+	mpz_t p;
+	mpz_t q;
+	mpz_t n;
+	mpz_t e;
+	mpz_t d;
+	mpz_t psub1;
+	mpz_t qsub1;
+	mpz_t phin;
+	mpz_t exponent1;
+	mpz_t exponent2;
+	mpz_t coefficient;
+	mpz_t tmp;
+
+	// 変数初期化
+	mpz_inits(p, q, n, e, d, psub1, qsub1, phin, exponent1, exponent2, coefficient, tmp, NULL);
+	// 素数探索
+	mpz_nextprime(tmp, num[0]);
+	fprintf(stderr, "p is prime\n");
+	// pへ素数をセット
+	mpz_set(tmp, p);
+	// 素数探索
+	mpz_nextprime(tmp, num[1]);
+	fprintf(stderr, "q is prime\n");
+	// 素数をセット
+	mpz_set(tmp, q);
+	// 使い終わったtmpをクリア
+	mpz_clear(tmp);
+	// nを算出
+	mpz_mul(n, p, q);
+	fprintf(stderr, "n is %ld bits\n", mpz_sizeinbase(n, 2));
+	// 公開鍵 e
+	mpz_set_ui(e, 65537UL);
+	// required p > q
+	if(mpz_cmp(p, q) <= 0){
+		mpz_swap(p, q);
+		fputs("p and q is swaped.", stderr);
+	}
+	// p - 1とq - 1を計算
+	mpz_sub_ui(psub1, p, 1);
+	mpz_sub_ui(qsub1, q, 1);
+	// phi(n) を計算
+	mpz_mul(phin, psub1, qsub1);
+	// 秘密鍵dを計算
+	mpz_invert(d, e, phin);
+	// phi(n)をクリア
+	mpz_clear(phin);
+	fputs("d", stderr);
+	mpz_mod(exponent1, d, psub1);
+	mpz_mod(exponent2, d, qsub1);
+	mpz_clears(psub1, qsub1, NULL);
+	mpz_invert(coefficient, q, p);
+	fputs("cof", stderr);
+
+	mpz_out_str(stdout, 16, n);
+	puts("");
+	mpz_out_str(stdout, 16, e);
+	puts("");
+	mpz_out_str(stdout, 16, d);
+	puts("");
+	mpz_out_str(stdout, 16, p);
+	puts("");
+	mpz_out_str(stdout, 16, q);
+	puts("");
+	mpz_out_str(stdout, 16, exponent1);
+	puts("");
+	mpz_out_str(stdout, 16, exponent2);
+	puts("");
+	mpz_out_str(stdout, 16, coefficient);
+	puts("");
+
+	mpz_clears(n, e, d, p, q, exponent1, exponent2, coefficient, NULL);
+
 	return 0;
 }
+
