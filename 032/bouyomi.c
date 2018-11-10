da26b69d841969542117837ef899ecb566f74aa0
Teruteru <teruterubouzu1024@gmail.com>
2018-11-10 17:55:31 +0900

    Create Bouyomichan remote reading tool

--- 032/bouyomi.c
+++ 032/bouyomi.c
@@ -4,14 +4,23 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h> //atoi(), exit(), EXIT_FAILURE, EXIT_SUCCESS
 #include <string.h> //memset(), strcmp()
 #include <unistd.h> //close()
+#include <iconv.h>
+#include <limits.h>
+#include <locale.h>
+#include <wchar.h>
 
 #define MSGSIZE 1024
 #define BUFSIZE (MSGSIZE + 1)
+#define DEFAULT_PORT 50001
+#define DEFAULT_SERV_ADDRESS "localhost"
+#define DEFAULT_SERV_ADDRESS4 "127.0.0.1"
+#define DEFAULT_SERV_ADDRESS6 "127.0.0.1"
 
 int main(int argc, char* argv[]){
     int sock;
@@ -20,14 +29,19 @@ int main(int argc, char* argv[]){
     char recvBuffer[BUFSIZE];
     char sendBuffer[BUFSIZE];
 
+    if(setlocale(LC_CTYPE, "") == NULL){
+    	perror("setlocale");
+	return EXIT_FAILURE;
+    }
+
     if (argc != 3) {
             fprintf(stderr, "argument count mismatch error.\n");
             exit(EXIT_FAILURE);
     }
 
-        memset(&servSockAddr, 0, sizeof(servSockAddr));
+    memset(&servSockAddr, 0, sizeof(servSockAddr));
 
-        servSockAddr.sin_family = AF_INET;
+    servSockAddr.sin_family = AF_INET;
 
 	// IPアドレスをバイナリに変換
     if (inet_aton(argv[1], &servSockAddr.sin_addr) == 0) {
@@ -39,7 +53,7 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "invalid port number.\n");
         exit(EXIT_FAILURE);
     }
-        servSockAddr.sin_port = htons(servPort);
+    servSockAddr.sin_port = htons(servPort);
 
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ){
         perror("socket() failed.");
@@ -53,43 +67,63 @@ int main(int argc, char* argv[]){
     // コネクションの確立ここまで
     printf("connect to %s\n", inet_ntoa(servSockAddr.sin_addr));
 
-    while(1){
-        printf("please enter the characters:");
-        if (fgets(sendBuffer, BUFSIZE, stdin) == NULL) {
-            fprintf(stderr, "invalid input string.\n");
-            exit(EXIT_FAILURE);
-        }
-
-    if (send(sock, sendBuffer, strlen(sendBuffer), 0) <= 0) {
-        perror("send() failed.");
-        exit(EXIT_FAILURE);
+    // 読み上げさせるメッセージ
+    wchar_t message[] = L"よかったのかホイホイついてきて\n俺はノンケだってかまわないで食っちまう人間なんだぜ";
+    // マルチバイト文字列のメモリ領域を確保
+    size_t mb_size = MB_LEN_MAX * wcslen(message) + 1;
+    char *mb = malloc(mb_size);
+    if(mb == NULL){
+    	perror("malloc");
+	return EXIT_FAILURE;
+    }
+    // 文字コード変換
+    if(wcstombs(mb, message, mb_size) == -1){
+    	perror("wcstombs");
+	return EXIT_FAILURE;
     }
 
-    int byteRcvd  = 0;
-    int byteIndex = 0;
-    while (byteIndex < MSGSIZE) {
-        byteRcvd = recv(sock, &recvBuffer[byteIndex], 1, 0);
-        if (byteRcvd > 0) {
-            if (recvBuffer[byteIndex] == '\n') {
-                recvBuffer[byteIndex] = '\0';
-                if (strcmp(recvBuffer, "quit") == 0) {
-                    close(sock);
-                    return EXIT_SUCCESS;
-                } else {
-                        break;
-                }
-            }
-            byteIndex += byteRcvd;
-         } else if(byteRcvd == 0) {
-            perror("ERR_EMPTY_RESPONSE");
-            exit(EXIT_FAILURE);
-         } else {
-            perror("recv() failed.");
-            exit(EXIT_FAILURE);
-         }
-     }
-        printf("server return: %s\n", recvBuffer);
-  }
+    // 棒読みちゃん向けにエンコード
+    size_t capacity = 0;
+    short command = 1;
+    short speed = -1;
+    short tone = -1;
+    short volume = -1;
+    short voice = 0;
+    char encode = 0;
+    size_t length = strlen(mb);
+    // なぜhtonsなしで読み上げできるのか謎
+    //command = (short) htons(command);
+    capacity += sizeof(command);
 
+    //speed = (short) htons(speed);
+    capacity += sizeof(speed);
+
+    //tone = (short) htons(tone);
+    capacity += sizeof(tone);
+
+    //volume = (short) htons(volume);
+    capacity += sizeof(volume);
+
+    capacity += sizeof(encode);
+
+    capacity += length;
+
+    int length_32 = (uint32_t)length;
+    //length_32 = (int) htonl((uint32_t)length);
+    capacity += sizeof(length);
+
+    ssize_t w = 0;
+    // 送信
+    w = write(sock, &command, sizeof(command));
+    w = write(sock, &speed, sizeof(speed));
+    w = write(sock, &tone, sizeof(tone));
+    w = write(sock, &volume, sizeof(volume));
+    w = write(sock, &voice, sizeof(voice));
+    w = write(sock, &encode, sizeof(encode));
+    w = write(sock, &length_32, sizeof(length_32));
+    w = write(sock, mb, length);
+
+    // ソケットを閉じる
+    close(sock);
     return EXIT_SUCCESS;
 }
