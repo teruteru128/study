194e5c9a47e06bb86798f7fc62b5a2b52c42a472
Teruteru <teruterubouzu1024@gmail.com>
2018-11-12 21:08:05 +0900

    Update to read message commandline args

--- 032/bouyomi.c
+++ 032/bouyomi.c
@@ -18,23 +18,24 @@
 #define MSGSIZE 1024
 #define BUFSIZE (MSGSIZE + 1)
 #define DEFAULT_PORT 50001
+#define DEFAULT_PORT_STR "50001"
 #define DEFAULT_SERV_ADDRESS "localhost"
 #define DEFAULT_SERV_ADDRESS4 "127.0.0.1"
-#define DEFAULT_SERV_ADDRESS6 "127.0.0.1"
+#define DEFAULT_SERV_ADDRESS6 "::1"
 
 int main(int argc, char* argv[]){
     int sock;
     struct sockaddr_in servSockAddr;
     unsigned short servPort;
-    char recvBuffer[BUFSIZE];
-    char sendBuffer[BUFSIZE];
+    char servAddr[] = DEFAULT_SERV_ADDRESS4;
+    char servPortStr[] = DEFAULT_PORT_STR;
 
     if(setlocale(LC_CTYPE, "") == NULL){
         perror("setlocale");
         return EXIT_FAILURE;
     }
 
-    if (argc != 3) {
+    if (argc != 2) {
             fprintf(stderr, "argument count mismatch error.\n");
             exit(EXIT_FAILURE);
     }
@@ -44,12 +45,12 @@ int main(int argc, char* argv[]){
     servSockAddr.sin_family = AF_INET;
 
     // IPアドレスをバイナリに変換
-    if (inet_aton(argv[1], &servSockAddr.sin_addr) == 0) {
+    if (inet_aton(servAddr, &servSockAddr.sin_addr) == 0) {
         fprintf(stderr, "Invalid IP Address.\n");
         exit(EXIT_FAILURE);
     }
 
-    if ((servPort = (unsigned short) atoi(argv[2])) == 0) {
+    if ((servPort = (unsigned short) atoi(servPortStr)) == 0) {
         fprintf(stderr, "invalid port number.\n");
         exit(EXIT_FAILURE);
     }
@@ -67,20 +68,7 @@ int main(int argc, char* argv[]){
     // コネクションの確立ここまで
     printf("connect to %s\n", inet_ntoa(servSockAddr.sin_addr));
 
-    // 読み上げさせるメッセージ
-    wchar_t message[] = L"よかったのかホイホイついてきて\n俺はノンケだってかまわないで食っちまう人間なんだぜ";
-    // マルチバイト文字列のメモリ領域を確保
-    size_t mb_size = MB_LEN_MAX * wcslen(message) + 1;
-    char *mb = malloc(mb_size);
-    if(mb == NULL){
-        perror("malloc");
-    return EXIT_FAILURE;
-    }
-    // 文字コード変換
-    if(wcstombs(mb, message, mb_size) == -1){
-        perror("wcstombs");
-        return EXIT_FAILURE;
-    }
+    char *mb = argv[1];
 
     // 棒読みちゃん向けにエンコード
     size_t capacity = 0;
