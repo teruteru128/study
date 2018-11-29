bb94cf3cdac070a3f822d554ce74fd2e71bd9eaf
Teruteru <teruterubouzu1024@gmail.com>
2018-11-29 20:56:26 +0900

    Complete BitMessage XML-RPC sample client

--- 033/bm.c
+++ 033/bm.c
@@ -1,92 +1,98 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <stdlib.h> //atoi(), exit(), EXIT_FAILURE, EXIT_SUCCESS
-#include <string.h> //memset(), strcmp()
-#include <unistd.h> //close()
+#include <xmlrpc.h>
+#include <xmlrpc_client.h>
+#include <string.h>
 #include "bitmessage.h"
 #define localhost_ip "127.0.0.1"
 #define bitmessage_port 8442
+#define NAME "TR BM TEST CLIENT"
+#define VERSION "0.0.1-alpha"
+#define SERVER_URL "http://127.0.0.1:8442/"
 
-int msgsend(int sock, char* msg){
-	return write(sock, msg, strlen(msg));
+void die_if_fault_occurred (xmlrpc_env *env)
+{
+    /* Check our error-handling environment for an XML-RPC fault. */
+    if (env->fault_occurred) {
+        fprintf(stderr, "XML-RPC Fault: %s (%d)\n",
+                env->fault_string, env->fault_code);
+        exit(1);
+    }
 }
 
-int main(int argc, char* argv[]){
-	int sock;
-	struct sockaddr_in servSockAddr;
-	unsigned short servPort;
-	printf("%d\n", BM_init());
+int main(int const argc, const char ** const argv) {
 
-	memset(&servSockAddr, 0, sizeof(servSockAddr));
-	servSockAddr.sin_family = AF_INET;
+    xmlrpc_env env;
+    xmlrpc_value * resultP = NULL;
+    xmlrpc_server_info *sinfo = NULL;
+    const char * msg = NULL;
+    char * const clientName = NAME;
+    char * const clientVersion = VERSION;
+    char * const url = SERVER_URL;
+    char * const methodName = "helloWorld";
+    xmlrpc_client* cp = NULL;
+    xmlrpc_value* paramArray;
+    xmlrpc_value* first;
+    xmlrpc_value* second;
 
-	if(inet_aton(localhost_ip, &servSockAddr.sin_addr) == 0){
-		fprintf(stderr, "\n");
-		perror("inet_aton");
-		return EXIT_FAILURE;
-	}
-	servSockAddr.sin_port = htons(bitmessage_port);
+    /* Initialize our error-handling environment. */
+    xmlrpc_env_init(&env);
 
-	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
-		perror("socket");
-		return EXIT_FAILURE;
-	}
+    /* Start up our XML-RPC client library. */
+    //xmlrpc_client_init2(&env, XMLRPC_CLIENT_NO_FLAGS, clientName, clientVersion, NULL, 0);
+    xmlrpc_client_setup_global_const(&env);
+    //[ handle possible failure of above ]
 
-	if (connect(sock, (struct sockaddr*) &servSockAddr, sizeof(servSockAddr)) < 0) {
-		perror("connect() failed.");
-		exit(EXIT_FAILURE);
-	}
-	printf("connected.\n");
-	ssize_t w = 0;
-	msgsend(sock, "POST / HTTP/1.0\r\n");
-	msgsend(sock, "Authorization: Basic dGVydXRlcnUxMjg6Xjs9NVwke0JxTzppdF5ranV3WEU4ZkMuVS0hWylJXUBM\r\n");
-	//msgsend(sock, "HOST: localhost\r\n");
-	//msgsend(sock, "Connection: close\r\n");
-	msgsend(sock, "\r\n");
-	msgsend(sock, "<?xml version=\"1.0\"?>\r\n");
-	msgsend(sock, "<methodCall>\r\n");
-	msgsend(sock, "  <methodName>helloWorld</methodName>\r\n");
-	msgsend(sock, "  <params>\r\n");
-	msgsend(sock, "    <param>\r\n");
-	msgsend(sock, "      <value>\r\n");
-	msgsend(sock, "        <struct>\r\n");
-	msgsend(sock, "          <member>\r\n");
-	msgsend(sock, "            <name>firstWord</name>\r\n");
-	msgsend(sock, "            <value><string>123</string></value>\r\n");
-	msgsend(sock, "          </member>\r\n");
-	msgsend(sock, "          <member>\r\n");
-	msgsend(sock, "            <name>secondWord</name>\r\n");
-	msgsend(sock, "            <value><string>456</string></value>\r\n");
-	msgsend(sock, "          </member>\r\n");
-	msgsend(sock, "        </struct>\r\n");
-	msgsend(sock, "      </value>\r\n");
-	msgsend(sock, "    </param>\r\n");
-	msgsend(sock, "  </params>\r\n");
-	msgsend(sock, "</methodCall>\r\n");
-	msgsend(sock, "\r\n");
-	int emptyLineCount = 0;
-	size_t msgsize = 1024;
-	size_t bufsize = msgsize + 1;
-	char buf[1025];
-	size_t len = 0;
-	
-	while((len = read(sock, buf, bufsize)) > 0){
-		printf("%s\n", buf);
-	}
-	/*
-	 *<?xml version="1.0"?>
-	 * <methodCall>
-	 *  <methodName>examples.getStateName</methodName>
-	 *  <params>
-	 *    <param>
-	 *     <value><i4>40</i4></value>
-	 *    </param>
-	 *  </params>
-	 *</methodCall>
-	 * */
-	close(sock);
-	return EXIT_SUCCESS;
+    xmlrpc_client_create(&env, XMLRPC_CLIENT_NO_FLAGS, clientName, clientVersion, NULL, 0, &cp);
+    die_if_fault_occurred(&env);
+
+    sinfo = xmlrpc_server_info_new(&env, url);
+    die_if_fault_occurred(&env);
+    xmlrpc_server_info_set_user(&env, sinfo, "teruteru128", "testpassword");
+    die_if_fault_occurred(&env);
+    xmlrpc_server_info_allow_auth_basic(&env, sinfo);
+
+    paramArray = xmlrpc_array_new(&env);
+    die_if_fault_occurred(&env);
+
+    first = xmlrpc_string_new(&env, "Hello");
+    die_if_fault_occurred(&env);
+
+    second = xmlrpc_string_new(&env, "World!");
+    die_if_fault_occurred(&env);
+
+    xmlrpc_array_append_item(&env, paramArray, first);
+    die_if_fault_occurred(&env);
+    xmlrpc_array_append_item(&env, paramArray, second);
+    die_if_fault_occurred(&env);
+
+    xmlrpc_client_call2(&env, cp, sinfo, methodName,
+                 paramArray, &resultP);
+    die_if_fault_occurred(&env);
+
+    //xmlrpc_parse_value(&env, resultP, "s", &msg);
+    xmlrpc_read_string(&env, resultP, &msg);
+    die_if_fault_occurred(&env);
+
+    printf("message : %s\n", msg);
+    
+    free((void *)msg);
+    msg = NULL;
+
+    xmlrpc_client_destroy(cp);
+    xmlrpc_DECREF(paramArray);
+    xmlrpc_server_info_free(sinfo);
+    /* Dispose of our result value. */
+    xmlrpc_DECREF(resultP);
+
+    /* Clean up our error-handling environment. */
+    xmlrpc_env_clean(&env);
+    
+    /* Shutdown our XML-RPC client library. */
+    //xmlrpc_client_cleanup();
+    xmlrpc_client_setup_global_const(&env);
+
+    return 0;
 }
+
