648198bd433654fb322cbdfec5e6bffba6f79c7b
Teruteru <teruterubouzu1024@gmail.com>
2018-12-20 20:52:14 +0900

    Delete duplicate file

--- 020/main.c
+++ 020/main.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 int main(int argc, char** argv){
-	setlocale (LC_ALL, "");
-	bindtextdomain (PACKAGE, LOCALDIR);
-	textdomain (PACKAGE);
-	printf(_("Hello world!\n"));
-	return 0;
+  setlocale (LC_ALL, "");
+  bindtextdomain (PACKAGE, LOCALDIR);
+  textdomain (PACKAGE);
+  printf(_("Hello world!\n"));
+  return 0;
 }
