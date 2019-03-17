51d99d242340cc531bf62e5e023bf9c98b6c0eb5
Teruteru <teruterubouzu1024@gmail.com>
2019-03-17 20:44:44 +0900

    Add pthread larning 1 and 2

---
+++ src/firstThread.c
@@ -0,0 +1,36 @@
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/*
+Linuxとpthreadによるマルチスレッドプログラミング入門 - 渋谷克智
+*/
+void *threadFunc(void *arg){
+  int i;
+  for(i = 0;i < 3; i++){
+    printf("I'm threadFunc.(%d)\n", i);
+    sleep(1);
+  }
+
+  return NULL;
+}
+
+int main(int argc, char* argv[]){
+  pthread_t thread;
+  int i=0;
+
+  if(pthread_create(&thread, NULL, threadFunc, NULL) != 0){
+    perror("failed to create new thread");
+    return EXIT_FAILURE;
+  }
+  sleep(1);
+
+  for(i = 0; i < 5; i++){
+    printf("I'm main. (%d)\n", i);
+    sleep(1);
+  }
+
+  return EXIT_SUCCESS;
+}
+
