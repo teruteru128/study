d3001a5664bc1959a60b338dc8be0821e7e35c41
teruteru128 <teruterubouzu1024@gmail.com>
2020-01-21 00:39:08 +0900

    Update

--- src/log-sample.c
+++ src/log-sample.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 int main(int argc, char* argv[]){
-    double a = log(114514);
+    double a = log(1<<18);
     double b = log(10);
     printf("%.24lf\n", a/b);
     printf("%.24lf\n", pow(10, a/b));
