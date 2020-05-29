709993fc0def1e9e1629386fea8739296832d300
Teruteru <teruterubouzu1024@gmail.com>
2020-05-29 17:21:18 +0900

    Update

--- src/019.c
+++ src/019.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define SIZE 65536
+
 int main(int argc, char **argv)
 {
     size_t i = 0;
     size_t j = 0;
     size_t sum = 0;
-    size_t size = 65536;
-    for(i = 0; i < size; i++)
+    for(i = 0; i < SIZE; i++)
     {
         sum++;
         for(j = 0; j < i; j++)
