6c7574d50f514972ec3cabe7cff2973c98e5630f
teruteru128 <teruterubouzu1024@gmail.com>
2020-05-29 22:58:50 +0900

    Update

--- src/gettextsample.h
+++ src/gettextsample.h
@@ -1,9 +1,7 @@
 
 #ifndef MAIN_H
 #define MAIN_H
-#ifdef HAVE_CONFIG_H
 #include "study-config.h"
-#endif
 #include <stdlib.h>
 
 int helloWorld001();
--- src/gettextsample3.h
+++ src/gettextsample3.h
@@ -2,9 +2,7 @@
 #ifndef MAIN_022_H
 #define MAIN_022_H
 
-#ifdef HAVE_CONFIG_H
 #include "study-config.h"
-#endif
 #include <stdio.h>
 #include <unistd.h>
 
