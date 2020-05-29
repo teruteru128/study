a3e3a2cfa42b38c82221238e793b4a71bb6aa26f
Teruteru <teruterubouzu1024@gmail.com>
2020-05-29 17:51:07 +0900

    Update

--- src/019.c
+++ src/019.c
@@ -3,22 +3,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SIZE 65536
-
+/*
+y^2 + (x - 0.5)^2 = 1
+x^2 - x + y^2 = 0
+x^2 - x + 0.25 - 0.25+ y^2 = 0
+(x-0.5)^2 + y^2 = 0.25
+--
+y^2 + (x-1)^2 = 1
+y^2 = 1 - (x^2 -2x +1)
+y^2 = 2x - x^2
+y = sqrt(2x - x^2)
+y = sqrt((2 - x)x)
+--
+y = 2 sqrt(x - x^2)
+  = 2 sqrt((1 - x) x)
+*/
 int main(int argc, char **argv)
 {
-    size_t i = 0;
-    size_t j = 0;
-    size_t sum = 0;
-    for(i = 0; i < SIZE; i++)
+    double a = 0x1.14514p-1;
+    printf("%.18lf\n", a);
+    while(0 < a && a < 1)
     {
-        sum++;
-        for(j = 0; j < i; j++)
-        {
-            sum++;
-            sum++;
-        }
+        a = 2 * sqrt((1 - a) * a);
+        printf("%.18lf\n", a);
     }
-    printf("%ld\n", sum);
+    printf("\n");
     return EXIT_SUCCESS;
 }
