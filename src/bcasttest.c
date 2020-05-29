3808398b14b89a1eb68a5f831c5ae07d080de0cf
teruteru128 <teruterubouzu1024@gmail.com>
2020-05-29 13:53:44 +0900

    Update

--- src/bcasttest.c
+++ src/bcasttest.c
@@ -8,18 +8,22 @@
 #include <unistd.h>
 #include <string.h>
 
+/** https://www.geekpage.jp/programming/linux-network/broadcast.php */
 int main(int argc, char **argv)
 {
     int sock;
     struct sockaddr_in addr;
     char *msg = NULL;
+    int yes = 1;
     ssize_t r = 0;
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
 
     addr.sin_family = AF_INET;
     addr.sin_port = htons(12345);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char *)&yes, sizeof(yes));
 
     if (argc >= 2)
     {
