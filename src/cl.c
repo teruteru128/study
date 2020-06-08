24e0855fb15e0be8babb76b0cf9090e504b0da81
Teruteru <teruterubouzu1024@gmail.com>
2020-06-09 08:06:30 +0900

    Update

--- src/cl.c
+++ src/cl.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "print_addrinfo.h"
@@ -20,14 +21,15 @@
  */
 int main(int argc, char *argv[])
 {
-  struct addrinfo hints;    /* 取得したいアドレス情報を指示する */
-  struct addrinfo *res;     /* 取得したアドレス情報が返ってくる */
-  struct addrinfo *adrinf;  /* 接続要求時に使う */
+  struct addrinfo hints;   /* 取得したいアドレス情報を指示する */
+  struct addrinfo *res;    /* 取得したアドレス情報が返ってくる */
+  struct addrinfo *adrinf; /* 接続要求時に使う */
 
   int rc;
 
   /* 引数の数をチェックする */
-  if (argc != 3) {
+  if (argc != 3)
+  {
     fprintf(stderr, "usage: %s nodename servname\n", argv[0]);
     return -1;
   }
@@ -37,13 +39,15 @@ int main(int argc, char *argv[])
   hints.ai_socktype = SOCK_STREAM;
 
   rc = getaddrinfo(argv[1], argv[2], &hints, &res);
-  if (rc != 0) {
+  if (rc != 0)
+  {
     fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(rc));
     return -1;
   }
 
   /* 得られたアドレスすべてに対し接続を行う */
-  for (adrinf = res; adrinf != NULL; adrinf = adrinf->ai_next) {
+  for (adrinf = res; adrinf != NULL; adrinf = adrinf->ai_next)
+  {
 
     char buf[2048];
     int sock;
@@ -53,13 +57,15 @@ int main(int argc, char *argv[])
      * 接続要求をする。
      */
     sock = socket(adrinf->ai_family, adrinf->ai_socktype, adrinf->ai_protocol);
-    if (sock < 0) {
+    if (sock < 0)
+    {
       perror("socket()");
       continue;
     }
 
     rc = connect(sock, adrinf->ai_addr, adrinf->ai_addrlen);
-    if (rc < 0) {
+    if (rc < 0)
+    {
       perror("connect()");
       close(sock);
       continue;
@@ -69,13 +75,13 @@ int main(int argc, char *argv[])
     print_addrinfo(adrinf);
 
     /* サーバからの応答を表示する */
-    while (0 < (len = read(sock, buf, sizeof(buf)))) {
+    while (0 < (len = read(sock, buf, sizeof(buf))))
+    {
       printf("%.*s", len, buf);
     }
 
     close(sock);
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
-
