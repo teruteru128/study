d9d7fdec2fff6812bf989826f604f0cc1a12e23b
Teruteru <teruterubouzu1024@gmail.com>
2020-06-11 00:44:39 +0900

    Update

--- src/bm.h
+++ src/bm.h
@@ -4,6 +4,8 @@
 
 #include <openssl/ripemd.h>
 #include <openssl/sha.h>
+#include <xmlrpc.h>
+#include <xmlrpc_client.h>
 
 typedef struct
 {
@@ -24,6 +26,13 @@ typedef struct connectioninfo_t
   clientinfo client;
   serverinfo server;
 } connectioninfo;
+
+typedef struct bm_client_t
+{
+  xmlrpc_env *env;
+  xmlrpc_client *cp;
+  xmlrpc_server_info *sinfo;
+} bm_client_t;
 
 /* int ripe(ripectx, signpub, encpub) */
 int ripe(RIPE_CTX *, char *signpub, size_t offsetsingpub, size_t lengthsignpub, char *encpub, size_t offsetencpub, size_t lengthencpub);
--- src/bmapi.h
+++ src/bmapi.h
@@ -6,40 +6,39 @@
 #include <xmlrpc.h>
 #include <xmlrpc_client.h>
 
-int api_init(void);
-int api_cleanup();
-char *api_helloWorld(char*, char*);
-int api_add(int, int);
-int api_statusBar();
-int api_listAddresses2();
-int api_createRandomAddress();
-char* api_getDeterministicAddress(char*, int, int);
-int api_getAllInboxMessages();
-int api_getAllInboxMessageIDs();
-int api_getSentMessageByAckData();
-int api_getAllSentMessages();
-int api_getSentMessageByID();
-int api_getSentMessagesBySender();
-int api_trashMessages();
+void bmapi_init(void);
+int bmapi_cleanup();
+char *bmapi_helloWorld(char*, char*);
+int bmapi_add(int, int);
+int bmapi_statusBar();
+int bmapi_listAddresses2();
+int bmapi_createRandomAddress();
+char* bmapi_getDeterministicAddress(char*, int, int);
+int bmapi_getAllInboxMessages();
+int bmapi_getAllInboxMessageIDs();
+int bmapi_getSentMessageByAckData();
+int bmapi_getAllSentMessages();
+int bmapi_getSentMessageByID();
+int bmapi_getSentMessagesBySender();
+int bmapi_trashMessages();
 /**
   simpleSendMessage(toaddress, fromaddress, subject, message)
   sendMessage(toAddress, fromAddress, subject, message, encodingType, TTL)
 */
-char* api_simpleSendMessage(char*, char*, char*, char*);
-char* api_sendMessage(char*, char*, char*, char*, int, int);
-int api_sendBroadcast();
-char* api_getStatus(char*);
-int api_listSubscriptions();
-int api_addSubscription();
-int api_deleteSubscriptions();
-int api_listAddressBookEntries();
-int api_addAddressBookEntry();
-int api_deleteAddressBookEntry();
-char *api_createChan(char *);
-int api_deleteAddress();
-int api_decodeAddress();
-int api_addAddressToBlackWhiteList();
-int api_removeAddressFromBlackWhiteList();
-int api_clientStatus();
+char* bmapi_simpleSendMessage(char*, char*, char*, char*);
+char* bmapi_sendMessage(char*, char*, char*, char*, int, int);
+int bmapi_sendBroadcast();
+char* bmapi_getStatus(char*);
+int bmapi_listSubscriptions();
+int bmapi_addSubscription();
+int bmapi_deleteSubscriptions();
+int bmapi_listAddressBookEntries();
+int bmapi_addAddressBookEntry();
+int bmapi_deleteAddressBookEntry();
+char *bmapi_createChan(char *);
+int bmapi_deleteAddress();
+int bmapi_decodeAddress();
+int bmapi_addAddressToBlackWhiteList();
+int bmapi_removeAddressFromBlackWhiteList();
+int bmapi_clientStatus();
 #endif
-
