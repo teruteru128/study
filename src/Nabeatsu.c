982607f1eab653d70f734874bfc8b44d796ae9a7
teruteru128 <teruterubouzu1024@gmail.com>
2020-06-11 14:07:25 +0900

    Update

--- src/Nabeatsu.c
+++ src/Nabeatsu.c
@@ -33,32 +33,5 @@ int main(int argc, char *argv[])
   setlocale(LC_ALL, "");
   bindtextdomain(PACKAGE, LOCALEDIR);
   textdomain(PACKAGE);
-  if (strstr(argv[0], "Nabeatsu"))
-  {
-    showNabeatsu();
-  }
-  else
-  {
-    int a = 334;
-    char b[64];
-    itoa(a, b, 10);
-    fputs(b, stdout);
-    fputs("\n", stdout);
-    int i = 0, max = 1000000;
-    struct timespec start;
-    clock_gettime(CLOCK_REALTIME, &start);
-    for (; i < MAX; i++)
-    {
-      itoa(i, b, 10);
-    }
-    struct timespec end;
-    clock_gettime(CLOCK_REALTIME, &end);
-    time_t sec = end.tv_sec - start.tv_sec;
-    long nsec = end.tv_nsec - start.tv_nsec;
-    double passed = (sec * 1e9) + nsec;
-    double seconds = passed / 1e9;
-    fprintf(stderr, _("It took %.8f seconds.\n"), seconds);
-    fprintf(stderr, _("%.1f times per second\n"), MAX / seconds);
-    fprintf(stderr, _("%.12f seconds per time\n"), seconds / MAX);
-  }
+  showNabeatsu();
 }
