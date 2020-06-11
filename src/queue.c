bc0ff5aa2257e77dbd72b078e418943c49eb269a
teruteru128 <teruterubouzu1024@gmail.com>
2020-06-11 13:34:13 +0900

    Format

--- src/queue.c
+++ src/queue.c
@@ -1,21 +1,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef struct a{
+typedef struct a
+{
     int64_t seed;
     int32_t x;
     int32_t z;
-}chunk_pos;
+} chunk_pos;
 // TODO chunk_pos* を void* にする
-typedef struct seedqueue{
-    chunk_pos* data;
+typedef struct seedqueue
+{
+    chunk_pos *data;
     size_t size;
     size_t head;
     size_t tail;
-}seedqueue;
+} seedqueue;
 
 //リングバッファ
-int enq(seedqueue* queue, chunk_pos* pos){}
-chunk_pos* deq(seedqueue* queue){}
-int init_queue(seedqueue* queue, size_t size){}
-int destry_queue(seedqueue* queue){}
+int enq(seedqueue *queue, chunk_pos *pos) {}
+chunk_pos *deq(seedqueue *queue) {}
+int init_queue(seedqueue *queue, size_t size) {}
+int destry_queue(seedqueue *queue) {}
