7ea38ecd6fec1879167e5ebe9813c330d524c990
Teruteru <teruterubouzu1024@gmail.com>
2020-06-11 16:08:36 +0900

    Update

--- src/itoabench.c
+++ src/itoabench.c
@@ -8,31 +8,51 @@
 #include <string.h>
 #include "printint.h"
 
-#define MAX (100000000)
+#define MAX (100000000L)
 int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "");
   bindtextdomain(PACKAGE, LOCALEDIR);
   textdomain(PACKAGE);
-  int a = 334;
+  int a = 0x114514;
   char b[64];
   itoa(a, b, 10);
   fputs(b, stdout);
   fputs("\n", stdout);
-  int i = 0, max = 1000000;
+  long i = 0;
   struct timespec start;
-  clock_gettime(CLOCK_REALTIME, &start);
-  for (; i < MAX; i++)
-  {
-    itoa(i, b, 10);
-  }
   struct timespec end;
+  time_t sec;
+  long nsec;
+  double passed;
+  double seconds;
+  clock_gettime(CLOCK_REALTIME, &start);
+  for (i = 0; i < MAX; i++)
+  {
+    ltoa(a, b, 10);
+  }
   clock_gettime(CLOCK_REALTIME, &end);
-  time_t sec = end.tv_sec - start.tv_sec;
-  long nsec = end.tv_nsec - start.tv_nsec;
-  double passed = (sec * 1e9) + nsec;
-  double seconds = passed / 1e9;
+  sec = end.tv_sec - start.tv_sec;
+  nsec = end.tv_nsec - start.tv_nsec;
+  passed = (sec * 1e9) + nsec;
+  seconds = passed / 1e9;
   fprintf(stderr, _("It took %.8f seconds.\n"), seconds);
   fprintf(stderr, _("%.1f times per second\n"), MAX / seconds);
   fprintf(stderr, _("%.12f seconds per time\n"), seconds / MAX);
+  snprintInt64(b, 64, a);
+  fputs(b, stdout);
+  fputs("\n", stdout);
+  clock_gettime(CLOCK_REALTIME, &start);
+  for (i = 0; i < 5000000000L; i++)
+  {
+    snprintInt64(b, 64, a);
+  }
+  clock_gettime(CLOCK_REALTIME, &end);
+  sec = end.tv_sec - start.tv_sec;
+  nsec = end.tv_nsec - start.tv_nsec;
+  passed = (sec * 1e9) + nsec;
+  seconds = passed / 1e9;
+  fprintf(stderr, _("It took %.8f seconds.\n"), seconds);
+  fprintf(stderr, _("%.1f times per second\n"), 5000000000 / seconds);
+  fprintf(stderr, _("%.12f seconds per time\n"), seconds / 5000000000);
 }
--- src/printint.c
+++ src/printint.c
@@ -16,18 +16,18 @@
  */
 static size_t stringSize(const uint64_t l)
 {
-    size_t i;
-    uint64_t p = 10;
-    for (i = 1; i < 20; i++)
+  size_t i;
+  uint64_t p = 10;
+  for (i = 1; i < 20; i++)
+  {
+    if (l < p)
     {
-        if (l < p)
-        {
-            return i;
-        }
-        p = 10 * p;
+      return i;
     }
-    return 20;
-    //return stringSizeRec(l, 1, 10);
+    p = 10 * p;
+  }
+  return 20;
+  //return stringSizeRec(l, 1, 10);
 }
 
 /**
@@ -35,76 +35,79 @@ static size_t stringSize(const uint64_t l)
  */
 size_t snprintUInt64(char *restrict s, size_t n, uint64_t l)
 {
-    uint64_t tmp;
-    size_t length;
-    /* length = min(length, n); */
-    //length = length < n ? length : n;
-    uint64_t q;
-    uint32_t r;
-    size_t charPos;
-    size_t i;
+  uint64_t tmp;
+  size_t length;
+  /* length = min(length, n); */
+  //length = length < n ? length : n;
+  uint64_t q;
+  uint32_t r;
+  size_t charPos;
+  size_t i;
 
-    if (s == NULL || n == 0)
-    {
-        return 0;
-    }
+  if (s == NULL || n == 0)
+  {
+    return 0;
+  }
 
-    tmp = l;
-    memset(s, 0, n);
-    length = stringSize(tmp);
-    for (i = length; i > n; i--)
-    {
-        tmp = tmp / 10;
-    }
-    charPos = i;
+  tmp = l;
+  memset(s, 0, n);
+  length = stringSize(tmp);
+  for (i = length; i > n; i--)
+  {
+    tmp = tmp / 10;
+  }
+  charPos = i;
 
-    while (tmp > 0x7FFFFFFFULL)
-    {
-        q = tmp / 100;
-        r = (uint32_t)(tmp - ((q << 6) + (q << 5) + (q << 2)));
-        tmp = q;
-        s[--charPos] = DigitOnes[r];
-        s[--charPos] = DigitTens[r];
-    }
-    uint32_t q2;
-    uint32_t i2 = (uint32_t)tmp;
-    while (i2 >= 65536)
-    {
-        q2 = i2 / 100;
-        r = (i2 - ((q2 << 6) + (q2 << 5) + (q2 << 2)));
-        i2 = q2;
-        s[--charPos] = DigitOnes[r];
-        s[--charPos] = DigitTens[r];
-    }
+  while (tmp > 0x7FFFFFFFULL)
+  {
+    q = tmp / 100;
+    r = (uint32_t)(tmp - ((q << 6) + (q << 5) + (q << 2)));
+    tmp = q;
+    s[--charPos] = DigitOnes[r];
+    s[--charPos] = DigitTens[r];
+  }
+  uint32_t q2;
+  uint32_t i2 = (uint32_t)tmp;
+  while (i2 >= 65536)
+  {
+    q2 = i2 / 100;
+    r = (i2 - ((q2 << 6) + (q2 << 5) + (q2 << 2)));
+    i2 = q2;
+    s[--charPos] = DigitOnes[r];
+    s[--charPos] = DigitTens[r];
+  }
 
-    for (;;)
+  for (;;)
+  {
+    q2 = (i2 * 52429) >> (16 + 3);
+    r = i2 - ((q2 << 3) + (q2 << 1));
+    s[--charPos] = DIGITS[r];
+    i2 = q2;
+    if (i2 == 0)
     {
-        q2 = (i2 * 52429) >> (16 + 3);
-        r = i2 - ((q2 << 3) + (q2 << 1));
-        s[--charPos] = DIGITS[r];
-        i2 = q2;
-        if (i2 == 0)
-        {
-            break;
-        }
+      break;
     }
-    return i;
+  }
+  return i;
 }
 
 size_t snprintInt64(char *restrict s, size_t n, int64_t l)
 {
-    return 0;
+  return 0;
 }
 
-/*
-  TODO: 適切なモジュールへ移動
-  XXX: strlen(a) != nの場合の挙動は？
-*/
-char* reverse(char* a, size_t n){
+/**
+ * 文字列の順序を逆にします。
+ * TODO: 適切なモジュールへ移動
+ * XXX: strlen(a) != nの場合の挙動は？
+ */
+char *reverse(char *a, size_t n)
+{
   //size_t len = strlen(a);
   char tmp = 0;
   size_t start = 0, end = n;
-  while(start < end){
+  while (start < end)
+  {
     tmp = *(a + start);
     *(a + start) = *(a + end);
     *(a + end) = tmp;
@@ -114,10 +117,11 @@ char* reverse(char* a, size_t n){
   return a;
 }
 
-char* ltoa(long value, char *str, int radix){
+char *ltoa(long value, char *str, int radix)
+{
   size_t i = 0;
   bool isNegative = false;
-  
+
   /* Handle 0 explicitely, otherwise empty string is printed for 0 */
   if (value == 0)
   {
@@ -125,35 +129,36 @@ char* ltoa(long value, char *str, int radix){
     str[i] = '\0';
     return str;
   }
-  
-  // In standard itoa(), negative numbers are handled only with  
+
+  // In standard itoa(), negative numbers are handled only with
   // base 10. Otherwise numbers are considered unsigned.
   if (value < 0 && radix == 10)
   {
     isNegative = true;
     value = -value;
   }
-  
+
   // Process individual digits
   while (value != 0)
   {
     int rem = value % radix;
-    str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
-    value = value/radix;
+    str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+    value = value / radix;
   }
-  
+
   // If number is negative, append '-'
   if (isNegative)
     str[i++] = '-';
-  
+
   str[i] = '\0'; // Append string terminator
-  
+
   // Reverse the string
-  reverse(str, i-1);
+  reverse(str, i - 1);
   return str;
 }
 
-char* itoa(int value, char* str, int radix){
+char *itoa(int value, char *str, int radix)
+{
   int i = 0;
   bool isNegative = false;
   /* Handle 0 explicitely, otherwise empty string is printed for 0 */
@@ -163,28 +168,28 @@ char* itoa(int value, char* str, int radix){
     str[i] = '\0';
     return str;
   }
-  
-  // In standard itoa(), negative numbers are handled only with  
+
+  // In standard itoa(), negative numbers are handled only with
   // base 10. Otherwise numbers are considered unsigned.
   if (value < 0 && radix == 10)
   {
     isNegative = true;
     value = -value;
   }
-  
+
   // Process individual digits
   while (value != 0)
   {
     int rem = value % radix;
-    str[i++] = (rem > 9)? ((rem-10) + 'a') : (rem + '0');
-    value = value/radix;
+    str[i++] = (rem > 9) ? ((rem - 10) + 'a') : (rem + '0');
+    value = value / radix;
   }
   // If number is negative, append '-'
   if (isNegative)
     str[i++] = '-';
-  
+
   str[i] = '\0'; // Append string terminator
   // Reverse the string
-  reverse(str, i-1);
+  reverse(str, i - 1);
   return str;
 }
