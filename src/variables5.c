80f6b5b47b8a3348f0db89bdfc9af25044650f68
Teruteru <teruterubouzu1024@gmail.com>
2020-06-17 02:12:18 +0900

    Update variables5

--- src/variables5.c
+++ src/variables5.c
@@ -27,17 +27,16 @@ void anotherFunc()
 int main(int argc, char *argv[])
 {
     pthread_t thread1, thread2;
-    char varB[STRINGSIZE];
-    varA = varB;
 
+    anotherFunc();
     strcpy(varA, "Good morning");
-    printf("main-1: varA is '%s', varB is '%s'\n", varA, varB);
+    printf("main-1: varA is '%s'\n", varA);
     pthread_create(&thread1, NULL, threadFunc, (void *)1);
     sleep(1);
-    printf("main-2: varA is '%s', varB is '%s'\n", varA, varB);
+    printf("main-2: varA is '%s'\n", varA);
     pthread_create(&thread2, NULL, threadFunc, (void *)2);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-    printf("main-3: varA is '%s', varB is '%s'\n", varA, varB);
+    printf("main-3: varA is '%s'\n", varA);
     return EXIT_SUCCESS;
 }
