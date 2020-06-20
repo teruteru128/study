294d927a6474c8ecafc26612d7c9e324640c4c52
Teruteru <teruterubouzu1024@gmail.com>
2020-06-20 19:48:31 +0900

    Update

--- src/study.c
+++ src/study.c
@@ -17,10 +17,19 @@
  */
 int main(int argc, char *argv[])
 {
-  setlocale(LC_ALL, "");
-  bindtextdomain(PACKAGE, LOCALEDIR);
-  textdomain(PACKAGE);
-  printf(_("Help me!\n"));
-  orz(1);
+  //setlocale(LC_ALL, "");
+  //bindtextdomain(PACKAGE, LOCALEDIR);
+  //textdomain(PACKAGE);
+  //printf(_("Help me!\n"));
+  //orz(1);
+
+  int i = 0;
+  int under = 62;
+  double topsubunder = 0;
+  for(; i < 95; i++)
+  {
+    topsubunder = 10 + 2.5 * i;
+    printf("%c : %.1fcm ( + %.1f cm)\n", i >= 26 ? '?' : i + 'A', under + topsubunder, topsubunder);
+  }
   return EXIT_SUCCESS;
 }
