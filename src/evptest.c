258cc9eedb73f7b030166c7b6ec92756d5e5ca21
Teruteru <teruterubouzu1024@gmail.com>
2020-06-26 01:29:42 +0900

    Update evptest

--- src/evptest.c
+++ src/evptest.c
@@ -12,15 +12,26 @@ int main(int argc, char const *argv[])
 {
     OpenSSL_add_all_digests();
     EVP_MD_CTX *ctx = EVP_MD_CTX_create();
-    EVP_DigestInit(ctx, EVP_get_digestbynid(NID_sha3_256));
-    EVP_DigestUpdate(ctx, str, strlen(str));
+
     unsigned int mdsize = 0;
     unsigned char md[EVP_MAX_MD_SIZE];
+
+    EVP_DigestInit(ctx, EVP_get_digestbynid(NID_sha3_256));
+    EVP_DigestUpdate(ctx, str, strlen(str));
     EVP_DigestFinal(ctx, md, &mdsize);
     for(unsigned int i = 0; i < mdsize; i++){
-        printf("%02x", md[i]);
+        fprintf(stdout, "%02x", md[i]);
     }
-    puts("");
+    fputs("\n", stdout);
+
+    EVP_DigestInit(ctx, EVP_sha3_256());
+    EVP_DigestUpdate(ctx, str, strlen(str));
+    EVP_DigestFinal(ctx, md, &mdsize);
+    for(unsigned int i = 0; i < mdsize; i++){
+        fprintf(stdout, "%02x", md[i]);
+    }
+    fputs("\n", stdout);
+
     EVP_MD_CTX_free(ctx);
     EVP_cleanup();
     return 0;
