5b418917b9f99b61c2b42aa575e61c5665020360
Teruteru <teruterubouzu1024@gmail.com>
2020-06-30 00:03:13 +0900

    Update study.c

--- src/study.c
+++ src/study.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <malloc.h>
 #include "gettextsample.h"
 #include <printint.h>
 #include <random.h>
 #include <bitset.h>
 #include <orz.h>
+#include <openssl/evp.h>
 
 /**
  * --version
@@ -22,10 +24,25 @@ int main(int argc, char *argv[])
   bindtextdomain(PACKAGE, LOCALEDIR);
   textdomain(PACKAGE);
   printf(_("Help me!\n"));
-  orz(1);
-  for(int i = -8; i < 8; i++)
+  const EVP_MD *sha512 = EVP_sha512();
+  EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+  EVP_DigestInit(ctx, sha512);
+  EVP_DigestUpdate(ctx, "BIG PRECURE IS WATCHING YOU", strlen("BIG PRECURE IS WATCHING YOU"));
+  unsigned char md[EVP_MAX_MD_SIZE];
+  unsigned int len = 0;
+  EVP_DigestFinal(ctx, md, &len);
+  for(int i = 0; i < len; i++)
   {
-    printf("%d\n", !!i);
+    printf("%02x", md[i]);
   }
+  printf("\n");
+  EVP_DigestInit(ctx, sha512);
+  EVP_DigestUpdate(ctx, md, len);
+  EVP_DigestFinal(ctx, md, &len);
+  for(int i = 0; i < len; i++)
+  {
+    printf("%02x", md[i]);
+  }
+  printf("\n");
   return EXIT_SUCCESS;
 }
