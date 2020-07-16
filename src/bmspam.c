5bf811f1d1fff81d68bb54a0b07ac3bb55738114
Teruteru <teruterubouzu1024@gmail.com>
2020-07-16 16:14:14 +0900

    Update

--- src/bmspam.c
+++ src/bmspam.c
@@ -5,8 +5,8 @@
 #endif
 #include <stdio.h>
 #include <stdlib.h>
-#include <xmlrpc.h>
-#include <xmlrpc_client.h>
+#include <xmlrpc-c/base.h>
+#include <xmlrpc-c/client.h>
 #include <string.h>
 #include <uuid/uuid.h>
 #include <bitmessage.h>
@@ -18,6 +18,17 @@
 #define NAME "TR BM TEST CLIENT"
 #define SERVER_URL "http://127.0.0.1:8442/"
 #define GENELRAL "BM-2cW67GEKkHGonXKZLCzouLLxnLym3azS8r"
+
+static void die_if_fault_occurred(xmlrpc_env *env)
+{
+  /* Check our error-handling environment for an XML-RPC fault. */
+  if (env->fault_occurred)
+  {
+    fprintf(stderr, "XML-RPC Fault: %s (%d)\n",
+            env->fault_string, env->fault_code);
+    exit(1);
+  }
+}
 
 /**
  * int main(int argc, char* argv[]){
@@ -35,33 +46,49 @@
  * */
 int main(int const argc, const char **const argv)
 {
-  char buf[BUFSIZ];
-  char *sendmsg = "";
-  char chan_name[64];
-  char *msg = NULL;
-  bmapi_init();
-  if (argc < 2)
-  {
-    return EXIT_FAILURE;
-  }
-  FILE *infile = NULL;
-  infile = fopen(argv[1], "r");
-  if (infile == NULL)
-  {
-    perror("fopen");
-    return EXIT_FAILURE;
-  }
+  xmlrpc_env env;
+  xmlrpc_client *clientP;
+  xmlrpc_server_info *serverP;
+  xmlrpc_value *resultP;
+  const char *msg;
+  char *const methodName = "helloWorld";
 
-  bm_client *c = bm_client_new();
+  /* Initialize our error-handling environment. */
+  xmlrpc_env_init(&env);
 
-  char *tmp = NULL;
-  char *message = "";
-  do
-  {
-    tmp = fgets(chan_name, 64, infile);
-    msg = bmapi_simpleSendMessage(chan_name, GENELRAL, "HAPPY NEW YEAR!", message);
-    printf("%s\n", msg);
-  } while (tmp != NULL);
+  xmlrpc_client_setup_global_const(&env);
+
+  xmlrpc_client_create(&env, XMLRPC_CLIENT_NO_FLAGS, NAME, VERSION, NULL, 0,
+                       &clientP);
+  die_if_fault_occurred(&env);
+  serverP = xmlrpc_server_info_new(&env, SERVER_URL);
+  xmlrpc_server_info_set_user(&env, serverP, "teruteru128", "testpassword");
+  xmlrpc_server_info_allow_auth_basic(&env, serverP);
+
+  xmlrpc_value *paramArray = xmlrpc_array_new(&env);
+  xmlrpc_value *f = xmlrpc_string_new(&env, "hello");
+  xmlrpc_value *s = xmlrpc_string_new(&env, "world");
+  xmlrpc_array_append_item(&env, paramArray, f);
+  xmlrpc_array_append_item(&env, paramArray, s);
+
+  /* Make the remote procedure call */
+  xmlrpc_client_call2(&env, clientP, serverP, methodName, paramArray, &resultP);
+  die_if_fault_occurred(&env);
+
+  /* Get our sum and print it out. */
+  xmlrpc_read_string(&env, resultP, &msg);
+  die_if_fault_occurred(&env);
+  printf("The sum  is %s\n", msg);
+  free((void *)msg);
+
+  /* Dispose of our result value. */
+  xmlrpc_DECREF(paramArray);
+  xmlrpc_DECREF(resultP);
+
+  /* Clean up our error-handling environment. */
+  xmlrpc_env_clean(&env);
+
+  xmlrpc_client_destroy(clientP);
 
   xmlrpc_client_teardown_global_const();
 
