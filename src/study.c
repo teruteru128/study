07e5ad6150c724175353979078f95f1c00839c29
Teruteru <teruterubouzu1024@gmail.com>
2020-07-18 08:17:30 +0900

    Update

--- src/study.c
+++ src/study.c
@@ -5,6 +5,9 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define STR "abcdefgabcdefghij"
 
 /**
  * --version
@@ -24,7 +27,9 @@
  *   EVP_MAC_new_ctx
  * 鍵導出関数
  * strpbrk
+ *   文字検索関数
  * strsep
+ *   トークン分割(空フィールド対応版)
  * strspn
  */
 int main(int argc, char *argv[])
@@ -36,5 +41,19 @@ int main(int argc, char *argv[])
     v = argv[c];
     printf("%s\n", v);
   }
+  char search[21];
+  size_t p;
+
+  printf("文字群を入力しなさい。\n");
+  int r = scanf("%20s", search);
+  if (r == EOF)
+  {
+    perror("scanf");
+    return EXIT_FAILURE;
+  }
+
+  p = strspn(STR, search);
+  printf("%zd\n", p);
+
   return EXIT_SUCCESS;
 }
