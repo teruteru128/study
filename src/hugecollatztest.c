0305d35066e9faeb896e4548ae95415d8ff86b70
Teruteru <teruterubouzu1024@gmail.com>
2020-07-19 00:10:53 +0900

    Update

--- src/hugecollatztest.c
+++ src/hugecollatztest.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <openssl/bn.h>
 
+#define N "af240808297a359e600caae74b3b4edc7cbc3c451cbb2be0fe2902f95708a364" \
+          "851527f5f1adc831895d22e82aaaa642b38ff8b955b7b1b74bb3fe8f7e0757ec" \
+          "ef43db66621561cf600da4d8def8e0c362083d5413eb49ca59548526e52b8f1b" \
+          "9febf5a191c23349d843636a524bd28fe870514dd189697bc770f6b3dc1274db" \
+          "7b5d4b56d396bf1577a1b0f4a225f2af1c926718e5f40604ef90b9e400e4dd3a" \
+          "b519ff02baf43ceee08beb378becf4d7acf2f6f03dafdd759133191d1c40cb74" \
+          "24192193d914feac2a52c78fd50449e48d6347883c6983cbfe47bd2b7e4fc595" \
+          "ae0e9dd4d143c06773e314087ee53f9f73b8330acf5d3f3487968aee53e82515"
+
 /**
  * collatz huge integer test
  * */
@@ -31,7 +40,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    success = BN_hex2bn(&work, "af240808297a359e600caae74b3b4edc7cbc3c451cbb2be0fe2902f95708a364851527f5f1adc831895d22e82aaaa642b38ff8b955b7b1b74bb3fe8f7e0757ecef43db66621561cf600da4d8def8e0c362083d5413eb49ca59548526e52b8f1b9febf5a191c23349d843636a524bd28fe870514dd189697bc770f6b3dc1274db7b5d4b56d396bf1577a1b0f4a225f2af1c926718e5f40604ef90b9e400e4dd3ab519ff02baf43ceee08beb378becf4d7acf2f6f03dafdd759133191d1c40cb7424192193d914feac2a52c78fd50449e48d6347883c6983cbfe47bd2b7e4fc595ae0e9dd4d143c06773e314087ee53f9f73b8330acf5d3f3487968aee53e82515");
+    success = BN_hex2bn(&work, N);
     if (!success)
     {
         perror(NULL);
