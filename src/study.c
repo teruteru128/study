4ec31511a5341d8e0a5c312242630fbd864baf3d
Teruteru <teruterubouzu1024@gmail.com>
2020-07-21 01:07:32 +0900

    Update

--- src/study.c
+++ src/study.c
@@ -2,39 +2,13 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-#include "gettext.h"
-#define _(str) gettext(str)
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define STR "abcdefgabcdefghij"
-
-int strspntest(int argc, char **argv)
-{
-  int c = 0;
-  char *v = NULL;
-  for (c = 1; c < argc; c++)
-  {
-    v = argv[c];
-    printf("%s\n", v);
-  }
-  char search[21];
-  size_t p;
-
-  printf("文字群を入力しなさい。\n");
-  int r = scanf("%20s", search);
-  if (r == EOF)
-  {
-    perror("scanf");
-    return EXIT_FAILURE;
-  }
-
-  p = strspn(STR, search);
-  printf("%zd\n", p);
-  return EXIT_SUCCESS;
-}
+#include "gettext.h"
+#define _(str) gettext(str)
+#include <locale.h>
+#include "strspntest.h"
 
 /**
  * --version
