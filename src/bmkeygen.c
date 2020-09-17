4ee5216e2a39f61e7449d10b271d059d1e656a97
Teruteru <teruterubouzu1024@gmail.com>
2020-09-17 20:39:49 +0900

    Update

--- src/bmkeygen.c
+++ src/bmkeygen.c
@@ -15,6 +15,7 @@
 #include <random.h>
 #include <bm.h>
 #include <pthread.h>
+#include <omp.h>
 
 #define PRIVATE_KEY_LENGTH 32
 #define PUBLIC_KEY_LENGTH 65
@@ -33,7 +34,7 @@
         return EXIT_FAILURE;                                        \
     }
 
-static int calcRipe(EVP_MD_CTX *mdctx, const EVP_MD *sha512, const EVP_MD *ripemd160, char *cache64, char *pubSignKey, char *pubEncKey)
+static int calcRipe(EVP_MD_CTX *mdctx, const EVP_MD *sha512, const EVP_MD *ripemd160, unsigned char *cache64, unsigned char *pubSignKey, unsigned char *pubEncKey)
 {
     EVP_DigestInit(mdctx, sha512);
     EVP_DigestUpdate(mdctx, pubSignKey, PUBLIC_KEY_LENGTH);
@@ -51,73 +52,33 @@ struct searchArg
     size_t priKeyNmemb;
     unsigned char *publicKeys;
     size_t pubKeyNmemb;
-    size_t ibegin;
-    size_t iend;
-    size_t jbegin;
-    size_t jend;
+    size_t signBegin;
+    size_t signEnd;
+    size_t encBegin;
+    size_t encEnd;
     size_t minExportThreshold;
 };
 
-void *searchAddress(void *arg)
+struct results;
+struct results
 {
-    struct searchArg *searchArg = (struct searchArg *)arg;
-    if (!searchAddress)
-        return NULL;
-
-    unsigned char *privateKeys = searchArg->privateKeys;
-    unsigned char *publicKeys = searchArg->publicKeys;
-    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    const EVP_MD *sha512md = EVP_sha512();
-    const EVP_MD *ripemd160md = EVP_ripemd160();
-    unsigned char *iPublicKey;
-    unsigned char *jPublicKey;
-    unsigned char cache64[EVP_MAX_MD_SIZE];
-    size_t i = 0;
-    size_t iPubIndex = 0;
-    size_t ii = 0;
-    size_t j = 0;
-    size_t jIndex = 0;
-    size_t jj = 0;
-    int r = 0;
-    size_t nlz = 0;
-
-    for (i = 0; i < KEY_CACHE_SIZE; i++)
+    size_t signkeyindex;
+    size_t encryptkeyindex;
+    size_t numberOfLeadingZero;
+    struct results *next;
+};
+void freeresults(struct results *results)
+{
+    struct results *work = NULL;
+    while (results != NULL)
     {
-        iPubIndex = i * PUBLIC_KEY_LENGTH;
-        iPublicKey = publicKeys + iPubIndex;
-        // ヒープから直接参照するより一度スタックにコピーしたほうが早い説
-        calcRipe(mdctx, sha512md, ripemd160md, cache64, iPublicKey, iPublicKey);
-        if (!cache64[0])
-        {
-            for (nlz = 1; !cache64[nlz] && nlz < 20; nlz++)
-                ;
-            if (nlz >= REQUIRE_NLZ)
-                exportAddress(privateKeys + (i * PRIVATE_KEY_LENGTH), iPublicKey, privateKeys + (i * PRIVATE_KEY_LENGTH), iPublicKey, cache64);
-        }
-        for (j = 0; j < i; j++)
-        {
-            jPublicKey = publicKeys + (j * PUBLIC_KEY_LENGTH);
-            calcRipe(mdctx, sha512md, ripemd160md, cache64, iPublicKey, jPublicKey);
-            if (!cache64[0])
-            {
-                for (nlz = 1; !cache64[nlz] && nlz < 20; nlz++)
-                    ;
-                if (nlz >= REQUIRE_NLZ)
-                    exportAddress(privateKeys + (i * PRIVATE_KEY_LENGTH), iPublicKey, privateKeys + (j * PRIVATE_KEY_LENGTH), jPublicKey, cache64);
-            }
-            calcRipe(mdctx, sha512md, ripemd160md, cache64, jPublicKey, iPublicKey);
-            if (!cache64[0])
-            {
-                for (nlz = 1; !cache64[nlz] && nlz < 20; nlz++)
-                    ;
-                if (nlz >= REQUIRE_NLZ)
-                    exportAddress(privateKeys + (j * PRIVATE_KEY_LENGTH), jPublicKey, privateKeys + (i * PRIVATE_KEY_LENGTH), iPublicKey, cache64);
-            }
-        }
+        work = results->next;
+        free(results);
+        results = work->next;
     }
-    EVP_MD_CTX_free(mdctx);
-    return NULL;
 }
+
+pthread_mutex_t mutex;
 
 /**
  * TODO: リファクタリング
@@ -126,15 +87,22 @@ void *searchAddress(void *arg)
  */
 int main(int argc, char *argv[])
 {
+    #ifdef _OPENMP
+    printf("openmp\n");
+    #else
+    printf("no openmp\n");
+    #endif
     setlocale(LC_ALL, "");
     bindtextdomain(PACKAGE, LOCALEDIR);
     textdomain(PACKAGE);
+    pthread_mutex_init(&mutex, NULL);
     unsigned char *publicKeys = calloc(KEY_CACHE_SIZE, PUBLIC_KEY_LENGTH);
     if (!publicKeys)
     {
         perror("calloc(publicKeys)");
         return EXIT_FAILURE;
     }
+    fprintf(stderr, "calloced\n");
     {
         FILE *fin = fopen("publicKeys.bin", "rb");
         if (fin == NULL)
@@ -144,8 +112,12 @@ int main(int argc, char *argv[])
         }
         size_t l = fread(publicKeys, PUBLIC_KEY_LENGTH, KEY_CACHE_SIZE, fin);
         fclose(fin);
+        if (l == KEY_CACHE_SIZE)
+        {
+            fprintf(stderr, "ok\n");
+        }
     }
-    pthread_t pthreads[4];
+    fprintf(stderr, "loaded\n");
     /*
      * 67108864
      * 67108864(67108864+1)/2 = 2251799847239680
@@ -158,23 +130,49 @@ int main(int argc, char *argv[])
      * threads[2]:47453132<=x<58117980
      * threads[3]:58117980<=x<67108864
      */
-    size_t jbegins[4] = {0, 33554432, 47453132, 58117980};
-    size_t jends[4] = {33554432, 47453132, 58117980, 67108864};
-    struct searchArg arg[4];
-    for (size_t i = 0; i < 4; i++)
+
+    //unsigned char *privateKeys = NULL;
+    //unsigned char *publicKeys = ;
+    EVP_MD_CTX *mdctx = NULL;
+    const EVP_MD *sha512md = EVP_sha512();
+    const EVP_MD *ripemd160md = EVP_ripemd160();
+    unsigned char *signpubkey = NULL;
+    unsigned char *jPublicKey;
+    unsigned char cache64[EVP_MAX_MD_SIZE];
+    size_t iPubIndex = 0;
+    size_t ii = 0;
+    size_t jIndex = 0;
+    size_t jj = 0;
+    int r = 0;
+    size_t nlz = 0;
+
+    #pragma omp parallel for shared(sha512md, ripemd160md, publicKeys) private(cache64, signpubkey, mdctx)
+    for (size_t i = 0; i < 67108864; i++)
     {
-        arg[i].privateKeys = NULL;
-        arg[i].priKeyNmemb = 0;
-        arg[i].publicKeys = publicKeys;
-        arg[i].pubKeyNmemb = 67108864;
-        arg[i].ibegin = 0;
-        arg[i].iend = 67108864;
-        arg[i].jbegin = jbegins[i];
-        arg[i].jend = jends[i];
+        signpubkey = publicKeys + (i * PUBLIC_KEY_LENGTH);
+        #pragma omp parallel for shared(sha512md, ripemd160md, publicKeys, signpubkey) private(cache64, mdctx)
+        for (size_t j = 0; j < 67108864; j++)
+        {
+            mdctx = EVP_MD_CTX_new();
+            calcRipe(mdctx, sha512md, ripemd160md, cache64, signpubkey, publicKeys + (j * PUBLIC_KEY_LENGTH));
+            if (!cache64[0])
+            {
+                for (nlz = 1; !cache64[nlz] && nlz < 20; nlz++)
+                    ;
+                if (nlz >= REQUIRE_NLZ)
+                {
+                    //exportAddress(privateKeys + (i * PRIVATE_KEY_LENGTH), signpubkey, privateKeys + (j * PRIVATE_KEY_LENGTH), jPublicKey, cache64);
+                    pthread_mutex_lock(&mutex);
+                    fprintf(stderr, "%ld, %ld, %ld\n", nlz, i, j);
+                    pthread_mutex_unlock(&mutex);
+                }
+            }
+            EVP_MD_CTX_free(mdctx);
+        }
     }
-    /* DEAD CODE ***********************/
     //shutdown:
     //free(privateKeys);
     free(publicKeys);
+    pthread_mutex_destroy(&mutex);
     return EXIT_SUCCESS;
 }
