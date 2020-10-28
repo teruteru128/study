f7700318154072d4e45f6651e9c9d42ec56ed7a9
teruteru128 <teruterubouzu1024@gmail.com>
2020-10-28 13:46:15 +0900

    Update

--- src/log-sample.c
+++ src/log-sample.c
@@ -1,9 +1,11 @@
 
+#include <config.h>
 #include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <limits.h>
+#include <syslog.h>
 
 int main(int argc, char **argv)
 {
@@ -13,6 +15,9 @@ int main(int argc, char **argv)
     printf("%.24Lf\n", a);
     printf("%.24Lf\n", b);
     printf("%.24Lf\n", a / b);
-    printf("%.24Lf\n", powl(10, a / b));
+    printf("%.24Lf\n", powl(10, M_El));
+    long double c = LDBL_EPSILON;
+    printf("%La\n", c);
+    syslog(LOG_NOTICE, "HELLO WORLD");
     return EXIT_SUCCESS;
 }
