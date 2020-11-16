649ceb419faa291ccf3301bc0cb9a43c3a2fc7f5
teruteru128 <teruterubouzu1024@gmail.com>
2020-11-16 14:03:16 +0900

    Split bmspam.c

--- src/bmspam.c
+++ src/bmspam.c
@@ -17,81 +17,10 @@
 #include <time.h>
 #include <pthread.h>
 #include <errno.h>
-#define localhost_ip "127.0.0.1"
-#define bitmessage_port 8442
-#define NAME "TR BM TEST CLIENT"
-#define SERVER_URL "http://192.168.1.128:8442/"
-#define GENELRAL "BM-2cW67GEKkHGonXKZLCzouLLxnLym3azS8r"
+#include "bmspam.h"
 
 void die_if_fault_occurred(xmlrpc_env *env);
 
-#define SENDTO_ADDRESS_FILE "test.txt"
-#define SUBJECT "TWVycnkgQ2hyaXN0bWFzIQ=="
-#define MESSAGE "PHByZT4KICAgIEBAICAgICAgICAgIEBACiAgICBAQEAgICAgICAgIEBAQAogICAgQEBAQCAgICAg" \
-                "IEBAQEAgICBAQEBAICBAQCBAQEAgIEBAIEBAQCBAQCAgICAgIEBACiAgICBAQCBAQCAgICBAQCBA" \
-                "QCAgQEAgIEBAIEBAQCAgQEAgQEBAICBAQCBAQCAgICBAQAogICAgQEAgIEBAICBAQCAgQEAgQEBA" \
-                "QEBAICBAQCAgICAgIEBAICAgICAgIEBAICBAQAogICAgQEAgICBAQEBAICAgQEAgIEBAICAgICBA" \
-                "QCAgICAgIEBAICAgICAgICBAQEBACiAgICBAQCAgICBAQCAgICBAQCAgIEBAQEAgIEBAICAgICAg" \
-                "QEAgICAgICAgICBAQAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg" \
-                "ICBAQAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgQEAgIEBACiAgICAg" \
-                "ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgQEBAQAogICBAQEAgICBAQCAgICAg" \
-                "ICAgICAgICAgQEAgICAgICAgICAgQEAKIEBAICAgQEAgQEAgICAgICAgICAgICAgICAgICAgICAg" \
-                "ICAgIEBACkBAICAgICAgIEBAIEBAQCAgQEAgQEBAICBAQCAgIEBAQEAgQEBAQEBAICBAQCBAQCAg" \
-                "QEAgICAgICBAQEAgICAgQEBAQApAQCAgICAgICBAQEAgIEBAIEBAQCAgQEAgQEAgIEBAICAgICAg" \
-                "QEAgICAgQEBAICBAQCAgQEAgIEBAICBAQCAgQEAKQEAgICAgICAgQEAgICBAQCBAQCAgICAgIEBA" \
-                "ICAgQEBAICAgIEBAICAgIEBAICAgQEAgIEBAIEBAICAgQEAgICBAQEAKIEBAICAgQEAgQEAgICBA" \
-                "QCBAQCAgICAgIEBAICAgICBAQCAgIEBAIEBAIEBAICAgICAgIEBAIEBAICAgQEAgICAgIEBACiAg" \
-                "IEBAQCAgIEBAICAgQEAgQEAgICAgICAgQEAgQEBAQCAgICAgQEAgICBAQCAgICAgICBAQCAgQEBA" \
-                "QCBAQCBAQEBACgoKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg" \
-                "ICAgIHpyCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBZdCQk" \
-                "JC4KICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC4sZSQkJCQkRiYj" \
-                "MDM5OwogICAgICAgICAgICAgICAgICAgICAgICAgNGUgciAgICAgICAgICAgICAgICQkJCQkJCQu" \
-                "CiAgICAgICAgICAgICAgICAgICAgICAgICBkJCRiciAgICAgICAgICAgIF96JCQkJCQkJEZgCiAg" \
-                "ICAgICAgICAgICAgICAgICAgICAgICAgPyQkYi5fICAgICAgICAgIF4/JCQkJCQkJAogICAgICAg" \
-                "ICAgICAgICAgICAgICAgICAgICA0JCQkJnF1b3Q7ICAgICAtZWVjLiAgJnF1b3Q7JnF1b3Q7SlAm" \
-                "cXVvdDsgLi5lZWUkJS4uCiAgICAgICAgICAgICAgICAgICAgICAgICAgIC0qKk4gI2MgICAtXioq" \
-                "Ki5lRSAgXnokUCQkJCQkJCQkJHItCiAgICAgICAgICAgICAgICAgIC56ZSQkJCQkZXU/JGV1ICYj" \
-                "MDM5OyQkJCRiICQ9XiokJCAuJCQkJCQkJCQkJCZxdW90OwogICAgICAgICAgICAgICAtLS4mcXVv" \
-                "dDs/JCQkJCQkJCQkYyZxdW90OyQkYyAuJnF1b3Q7JnF1b3Q7JnF1b3Q7IGUkSyAgPSZxdW90OyZx" \
-                "dW90Oyo/JCQkUCZxdW90OyZxdW90OyZxdW90OyZxdW90OwogICB1ZWVlLiBgOmAgICRFICEhaCA/" \
-                "JCQkJCQkJCRiIFIkTiYjMDM5O34hISAqJCRGIEomcXVvdDsmcXVvdDsmcXVvdDtDLiAgYAogIEog" \
-                "IGAmcXVvdDskJGV1YCFoICEhIWA0ISEmbHQ7PyQkJCQkJCRQID8mcXVvdDsuZWVlLXouZWUmcXVv" \
-                "dDsgfiQkZS5icgogICYjMDM5O2okJE5lYD8kJGNgNCF+YC1lLTohOmAkJCQkJCQkICQkKiomcXVv" \
-                "dDt6ICReUiRQICAzICZxdW90OyQkJGJKCiAgIDQkJCRGJnF1b3Q7LmA/JCRjYCEhIFwpLiEhIWA/" \
-                "JCQkJEYuJCQkIyAkdSQlIGVlKiZxdW90O14gOjRgJnF1b3Q7JCZxdW90Oz8kcQogICAgJnF1b3Q7" \
-                "JnF1b3Q7YCwhISE6YCQkTi40ISF+fi5+fjQgPyQkRiYjMDM5OyQkRi5ALiogLUwuZUAkJCQkZWMu" \
-                "ICAgICAgJnF1b3Q7CiAgICAmcXVvdDtScmAhISEhaCA/JCRjYGg6IGAjICEhICRGLHI0JEwqKiog" \
-                "IGUkJCQkJCQkJCQkJCRoYwogICAgICAjZSYjMDM5OzQhISEhTGAkJGImIzAzOTshLjohaGB+fiAu" \
-                "JEYmIzAzOTsmcXVvdDsgICAgZCQkJCQkJCQkJCQkJCQkJCQkaCwKICAgICAgIF4kLmAhISEhaCAk" \
-                "JGJgIS4gLSAgICAkUCAvJiMwMzk7ICAgLiQkJCQkJCQkJCQkJCQkJCQkJCQkJGMKICAgICAgICAg" \
-                "JnF1b3Q7JGNgISEhaGAkJC40fiAgICAgICQkJHImIzAzOTsgICAmbHQ7JCQkJCQkJCQkJCQkJCQk" \
-                "JCQkJFAmcXVvdDsmcXVvdDsmcXVvdDsKICAgICAgICAgICBedGUuYH4gJCRiICAgICAgICBgRnVl" \
-                "LSAgIGAkJCQkJCQkJCQkJCQkJFAmcXVvdDsuOiAgISEgJnF1b3Q7Jmx0OwogICAgICAgICAgICAg" \
-                "IF4mcXVvdDs9NCRQJnF1b3Q7ICAgICAuLCwsLiAtXi4gICA/JCQkJCQkJCQkJCZxdW90Oz86LiAh" \
-                "ISA6ISF+ICwsZWMuLgogICAgICAgICAgICAgICAgICAgIC4ueiQkJCQkJCQkJGgsICAgIGAkJCQk" \
-                "JCRQJnF1b3Q7Li5gIWYgOiFmIH4pTHplJCQkUCZxdW90OyZxdW90OyZxdW90OyZxdW90Oz9pCiAg" \
-                "ICAgICAgICAgICAgICAgIHVkJCQkJCQkJCQkJCQkJCRoICAgIGA/JCRGICZsdDshISYjMDM5OyZs" \
-                "dDshJmd0Ozp+KXVlJCRQKiZxdW90Oy4uOiEhISEhIEoKICAgICAgICAgICAgICAgIC5LJCQkJCQk" \
-                "JCQkJCQkJCQkJCQsICAgICBQLiZndDtlJiMwMzk7IWYgIX4gZWQkJFAmcXVvdDsuOiEhISEhISEh" \
-                "YC5kJnF1b3Q7CiAgICAgICAgICAgICAgIHokJCQkJCQkJCQkJCQkJCQkJCQkJCAgICAgIDQhIX5c" \
-                "ZSQkJFBgOiEhISEhISEhISEmIzAzOTsuZVAmIzAzOTsKICAgICAgICAgICAgICAtKiZxdW90Oy4g" \
-                "LiAmcXVvdDs/PyQkJCQkJCQkJCQkJCAgICAgICB+IGB6JCQkRiZxdW90Oy5gISEhISEhISEhISYj" \
-                "MDM5OyxkUCZxdW90OwogICAgICAgICAgICAuJnF1b3Q7ICk6ISFoIGlgIS0gKCZxdW90Oz8kJCQk" \
-                "JCRmICAgICAgICAsJCRQJnF1b3Q7OiEgKS4gYCYjMDM5OyEhISFgLGQkRiYjMDM5OwogICAgICAg" \
-                "LnVlZWV1LkpgLV4uIWggJmx0Oy0gIH5gLi4gPz8kJCYjMDM5OyAgICAgICAsJCQgOiEhYGUkJCQk" \
-                "ZSBgLGUkRiYjMDM5OwogICAgZSQkJCQkJCQkJCQkJCRlZWlDICZxdW90Oyk/Oi0mbHQ7OiUmIzAz" \
-                "OTs6Xj8gICAgICAgID8kZiAhISEgPyQkJCQmcXVvdDssRiZxdW90OwogICBQJnF1b3Q7Li4uLmBg" \
-                "YCZxdW90OyZxdW90Oz8kJCQkJCQkJCRldUxeLiEuLmAgLiAgICAgICAgICZxdW90O1R1Ll8uLGBg" \
-                "JnF1b3Q7JnF1b3Q7CiAgICQgISEhISEhISEhITo6LiZxdW90OyZxdW90Oz8/JCQkJCQkZUp+Xj0u" \
-                "ICAgICAgICAgICAgYGBgYAogICA/JC5gISEhISEhISEhISEhISE6LiZxdW90Oz8/JCQkJCRjJiMw" \
-                "Mzk7LgogICAgJnF1b3Q7P2IuYCEhISEhISEhISEhISEhISEmZ3Q7LiZxdW90Oz8kJCQkYwogICAg" \
-                "ICBePyRjYCYjMDM5OyEhISEhISEhISEhJiMwMzk7LGVlYiwgJnF1b3Q7JCQkawogICAgICAgICAm" \
-                "cXVvdDs/JGUuYCYjMDM5OyEhISEhISEgJCQkJCQgOy4/JCQKICAgICAgICAgICAgJnF1b3Q7PyRl" \
-                "ZSxgYCYjMDM5OyYjMDM5OyEuJnF1b3Q7PyRQYGkhISAzUAogICAgICAgICAgICAgICAgJnF1b3Q7" \
-                "JnF1b3Q7Pz8kYmVjLCwuLGNlZWVQJnF1b3Q7CiAgICAgICAgICAgICAgICAgICAgICAgYCZxdW90" \
-                "OyZxdW90OyZxdW90OyZxdW90OyZxdW90OyZxdW90Owo8L3ByZT4="
-
-#define METHOD_NAME "sendMessage"
 #define USER_NAME "teruteru128"
 #define PASSWORD "testpassword"
 
@@ -118,14 +47,15 @@ int main(int const argc, const char **const argv)
   struct timespec currentTime;
   clock_gettime(CLOCK_REALTIME, &currentTime);
   currentTime.tv_nsec = 0;
-  struct tm *tm = localtime(&currentTime.tv_sec);
-  tm->tm_sec = 0;
-  tm->tm_min = 0;
-  tm->tm_hour = 4;
-  tm->tm_mday = 25;
-  tm->tm_mon = 11;
+  struct tm tm;
+  localtime_r(&currentTime.tv_sec, &tm);
+  tm.tm_sec = 0;
+  tm.tm_min = 0;
+  tm.tm_hour = 4;
+  tm.tm_mday = 25;
+  tm.tm_mon = 11;
   struct timespec christmasTime;
-  christmasTime.tv_sec = mktime(tm);
+  christmasTime.tv_sec = mktime(&tm);
   christmasTime.tv_nsec = 0;
   double diffsec = difftime(christmasTime.tv_sec, currentTime.tv_sec);
   long diffnsec = christmasTime.tv_nsec - currentTime.tv_nsec;
