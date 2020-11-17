9ce6a1234d6df58f30c289af8af24e21e48564ce
Teruteru <teruterubouzu1024@gmail.com>
2020-11-18 02:11:44 +0900

    Add SHA-1 RNG sample and command line arguments sample

--- src/study.c
+++ src/study.c
@@ -15,8 +15,29 @@
 #include <byteswap.h>
 #include <inttypes.h>
 #include <stddef.h>
+#include <openssl/evp.h>
 
 #define LIMIT 16
+
+typedef struct rndctx
+{
+  uint64_t count;
+  unsigned char ctx[20];
+} rndctx_t;
+
+int64_t next(rndctx_t *ctx)
+{
+  const EVP_MD *sha1 = EVP_sha1();
+  // 毎回ctxをnewしてfreeするのもったいないけどなにかいい方法ない？
+  EVP_MD_CTX *tmpmd = EVP_MD_CTX_new();
+  EVP_DigestInit(tmpmd, sha1);
+  *((uint64_t *)(ctx->ctx + 12)) = le64toh(ctx->count);
+  ctx->count++;
+  EVP_DigestUpdate(tmpmd, ctx->ctx, 20);
+  EVP_DigestFinal(tmpmd, ctx->ctx, NULL);
+  EVP_MD_CTX_free(tmpmd);
+  return  *((uint64_t *)(ctx->ctx + 12));
+}
 
 /**
  * --help
@@ -55,6 +76,19 @@ int main(int argc, char *argv[])
   setlocale(LC_ALL, "");
   bindtextdomain(PACKAGE, LOCALEDIR);
   textdomain(PACKAGE);
+  /*
+    SHA-1ベース乱数
+    内部状態 12バイト乱数+8バイトカウンタ(0スタート)
+    初期化 12バイト乱数
+    sha1(12バイト乱数+8バイトカウンタ)
+    →12バイト再利用+8バイト出力
+    カウンタをctxに上書き
+    カウンタをインクリメント
+    ハッシュしてctxに書き込む
+    出力値を返す
+  */
+  rndctx_t ctx;
+  ctx.count = 0;
   unsigned char buf[20];
   FILE *r = fopen("/dev/urandom", "rb");
   if (r == NULL)
@@ -65,7 +99,12 @@ int main(int argc, char *argv[])
   size_t len = fread(buf, sizeof(char), req, r);
   if (len != req)
   {
-    perror("fread error");
+    perror("fread error1");
+  }
+  len = fread(ctx.ctx, sizeof(char), req, r);
+  if (len != req)
+  {
+    perror("fread error2");
   }
   fclose(r);
   *((uint64_t *)(buf + 12)) = le64toh(1);
@@ -73,6 +112,26 @@ int main(int argc, char *argv[])
   {
     printf("%02x", buf[i]);
   }
-  fputs("\nひぐらしの\e[31mな\e[mく頃に\n", stdout);
+  fputs("\n", stdout);
+  for(int i = 0; i < 20; i++)
+  {
+    printf("%ld\n", next(&ctx));
+  }
+  int h = 0;
+  int v = 0;
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "--help") == 0)
+    {
+      h = 1;
+    }
+    else if (strcmp(argv[i], "--version") == 0)
+    {
+      v = 1;
+    }
+  }
+  printf("%d %d\n", h, v);
+  // メインコマンドのhelpとサブコマンドのhelpを別に実装するには？
+  fputs("ひぐらしの\e[31mな\e[mく頃に\n", stdout);
   return EXIT_SUCCESS;
 }
