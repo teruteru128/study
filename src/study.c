361a8bc3e3247bba3f077d62637c3ee06dde0f2f
teruteru128 <teruterubouzu1024@gmail.com>
2020-11-20 13:21:46 +0900

    Move content from study.c to timerfdsample.c

--- src/study.c
+++ src/study.c
@@ -4,32 +4,9 @@
 #endif
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include "gettext.h"
-#define _(str) gettext(str)
-#include <time.h>
-#include <pthread.h>
-#include <errno.h>
-#include <string.h>
-#include <stdint.h>
-#include <byteswap.h>
-#include <inttypes.h>
-#include <stddef.h>
-#include <openssl/evp.h>
-#include <sys/timerfd.h>
-#include <unistd.h>
-#include <netdb.h>
-
-#define LIMIT 16
 
 /**
- * --help
- * --verbose
- * --version
  * 
- * orz
- * 
- * OpenSSL EVP
  * 対称鍵暗号
  * 認証付き暗号
  * エンベロープ暗号化
@@ -51,57 +28,21 @@
  * geoip_load_file
  * https://youtu.be/MCzo6ZMfKa4
  * ターミュレーター
- * eventfd
- * timerfd
- * signalfd
  * 
- * Linuxにおけるウェイト処理
- * - sleep
- * - usleep
- * - nanosleep
- * - selectのタイムアウト指定
- * - settimer
- * - timerfd
- * - pthread_cond_timedwait
+ * regex.h
+ * - 最左最短一致
+ * - 否定先読み
+ * - 強欲な数量子
  * 
  * TODO: P2P地震情報 ピア接続受け入れ＆ピアへ接続
+ * 
+ * @brief sanbox func.
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
  */
 int main(int argc, char *argv[])
 {
-  struct timespec cur;
-  clock_gettime(CLOCK_REALTIME, &cur);
-  struct itimerspec spec;
-  spec.it_value.tv_sec = cur.tv_sec + 3;
-  spec.it_value.tv_nsec = cur.tv_nsec;
-  spec.it_interval.tv_sec = 1;
-  spec.it_interval.tv_nsec = 0;
-
-  int timer = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
-  if(timer < 0)
-  {
-    perror("timerfd_create");
-    return EXIT_FAILURE;
-  }
-  int ret = timerfd_settime(timer, TFD_TIMER_ABSTIME, &spec, NULL);
-  if(ret != 0)
-  {
-    perror("timerfd_settime");
-    close(timer);
-    return EXIT_FAILURE;
-  }
-
-  uint64_t numexpire = 0;
-  // recvで読み込むと失敗を返す
-  ssize_t r = read(timer, &numexpire, 8);
-  ret = 0;
-  if(r < 0)
-  {
-    perror("recv");
-    ret = EXIT_FAILURE;
-  }
-
-  printf("%" PRId64 "\n", numexpire);
-  close(timer);
-  fputs("ひぐらしの\x1b[31mな\x1b[mく頃に\n", stdout);
-  return ret;
+  return EXIT_SUCCESS;
 }
---
+++ src/timerfdsample.c
@@ -0,0 +1,95 @@
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "gettext.h"
+#define _(str) gettext(str)
+#include <time.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <byteswap.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <openssl/evp.h>
+#include <sys/timerfd.h>
+#include <unistd.h>
+#include <netdb.h>
+
+/**
+ * eventfd sample.
+ * 
+ * eventfd
+ * timerfd
+ * signalfd
+ * 
+ * Linuxにおけるウェイト処理
+ * - sleep
+ * - usleep
+ * - nanosleep
+ * - selectのタイムアウト指定
+ * - settimer
+ * - timerfd
+ * - pthread_cond_timedwait
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char *argv[])
+{
+    struct timespec cur;
+    clock_gettime(CLOCK_MONOTONIC, &cur);
+    printf("%ld.%09ld\n", cur.tv_sec, cur.tv_nsec);
+    printf("%lf\n", (double)cur.tv_sec / 86400);
+    clock_gettime(CLOCK_REALTIME, &cur);
+    struct itimerspec spec;
+    spec.it_value.tv_sec = cur.tv_sec + 3;
+    spec.it_value.tv_nsec = cur.tv_nsec;
+    // it_intervalを両方0にすると繰り返しタイマーオフ
+    spec.it_interval.tv_sec = 1;
+    spec.it_interval.tv_nsec = 0;
+
+    int timer = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
+    if (timer < 0)
+    {
+        perror("timerfd_create");
+        return EXIT_FAILURE;
+    }
+    int ret = timerfd_settime(timer, TFD_TIMER_ABSTIME, &spec, NULL);
+    if (ret != 0)
+    {
+        perror("timerfd_settime");
+        close(timer);
+        return EXIT_FAILURE;
+    }
+
+    // expiration
+    uint64_t exp = 0;
+    // max expiration
+    uint64_t max_exp = 16;
+    // total expiration
+    uint64_t tot_exp;
+    ssize_t r = 0;
+    ret = 0;
+    for (tot_exp = 0; tot_exp < max_exp;)
+    {
+        // recvで読み込むと失敗を返す
+        r = read(timer, &exp, sizeof(uint64_t));
+        if (r != sizeof(uint64_t))
+        {
+            perror("recv");
+            ret = EXIT_FAILURE;
+            break;
+        }
+        tot_exp += exp;
+        printf("read : %" PRId64 " , total : %" PRIu64 "\n", exp, tot_exp);
+    }
+
+    close(timer);
+    return ret;
+}
