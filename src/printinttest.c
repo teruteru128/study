d35714bbbb3fb0fdb37fd621f573c0c26615468b
Teruteru <teruterubouzu1024@gmail.com>
2020-11-30 12:02:32 +0900

    Update printinttest.c

--- src/printinttest.c
+++ src/printinttest.c
@@ -2,30 +2,55 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
 #include <printint.h>
 
 #define OUT_SIZE 21
 
+struct testcase
+{
+    uint64_t in;
+    const char *p;
+};
+
 int main(int argc, char **argv)
 {
-    uint64_t in = 1;
+    struct testcase testcase[] = {{0UL, "0"},
+                                  {1UL, "1"},
+                                  {10UL, "10"},
+                                  {100UL, "100"},
+                                  {1000UL, "1000"},
+                                  {10000UL, "10000"},
+                                  {100000UL, "100000"},
+                                  {1000000UL, "1000000"},
+                                  {10000000UL, "10000000"},
+                                  {100000000UL, "100000000"},
+                                  {1000000000UL, "1000000000"},
+                                  {10000000000UL, "10000000000"},
+                                  {100000000000UL, "100000000000"},
+                                  {1000000000000UL, "1000000000000"},
+                                  {10000000000000UL, "10000000000000"},
+                                  {100000000000000UL, "100000000000000"},
+                                  {1000000000000000UL, "1000000000000000"},
+                                  {10000000000000000UL, "10000000000000000"},
+                                  {100000000000000000UL, "100000000000000000"},
+                                  {1000000000000000000UL, "1000000000000000000"},
+                                  {10000000000000000000UL, "10000000000000000000"}};
     char out1[OUT_SIZE];
-    size_t i, length;
     memset(out1, 0, OUT_SIZE);
 
-    for (i = 1; i <= 20; i++, in *= 10)
+    for (size_t i = 0; i < 21; i++)
     {
-        length = snprintUInt64(out1, OUT_SIZE, in);
-        if (strncmp(out1, "10000000000000000000", length) == 0)
+        snprintUInt64(out1, OUT_SIZE, testcase[i].in);
+        if (strcmp(out1, testcase[i].p) != 0)
         {
-            printf("OK\n");
-        }
-        else
-        {
-            printf("NG\n");
+            fprintf(stdout, "NG : %lu\n", testcase[i].in);
+            return EXIT_FAILURE;
         }
     }
+    return EXIT_SUCCESS;
 }
