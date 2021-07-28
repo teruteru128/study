509df4ba4ddc5d75d54c8d907dbe60eab13a41df
Teruteru <teruterubouzu1024@gmail.com>
2021-07-29 02:35:33 +0900

    Update

--- src/centos_rip.c
+++ src/centos_rip.c
@@ -1,15 +1,17 @@
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <sys/timerfd.h>
 #include <time.h>
+#include <unistd.h>
 
 /**
  * @brief see https://centos.rip/
- * 
+ *
  */
-void CentOS_rip()
+int CentOS_rip()
 {
-    struct tm countDownTM;
+    struct tm countDownTM = { 0 };
     countDownTM.tm_sec = 0;
     countDownTM.tm_min = 0;
     countDownTM.tm_hour = 0;
@@ -19,49 +21,75 @@ void CentOS_rip()
     countDownTM.tm_wday = 0;
     countDownTM.tm_yday = 0;
     countDownTM.tm_isdst = 0;
-#ifdef __USE_MISC
-    countDownTM.tm_gmtoff = 0;
-    countDownTM.tm_zone = NULL;
-#else
-    countDownTM.__tm_gmtoff = 0;
-    countDownTM.__tm_zone = NULL;
-#endif
     const time_t countDownDate = mktime(&countDownTM);
-    struct timespec spec;
-    spec.tv_sec = 0;
-    // 1/10
-    // 0.010000000
-    //    10000000
-    // 1/20
-    // 0.005000000
-    //     5000000
-    // 1/128
-    // 0.007812500
-    //     7812500
-    spec.tv_nsec = 5000000L;
 
-    struct timespec nowspec;
     long secs = 0;
     long days;
     long hours;
     long minutes;
     long seconds;
     long milliseconds;
-    while (true)
+    struct timespec nowspec;
+    clock_gettime(CLOCK_REALTIME, &nowspec);
+
+    struct itimerspec timerspec = { 0 };
+    timerspec.it_value.tv_sec = nowspec.tv_sec;
+    timerspec.it_value.tv_nsec = nowspec.tv_nsec + 50000000;
+    if (timerspec.it_value.tv_nsec >= 1000000000)
     {
-        clock_gettime(CLOCK_REALTIME, &nowspec);
-        secs = (long)difftime(countDownDate, nowspec.tv_sec);
-        if (secs < 0)
-        {
-            break;
-        }
+        timerspec.it_value.tv_sec++;
+        timerspec.it_value.tv_nsec -= 1000000000;
+    }
+    timerspec.it_interval.tv_sec = 0;
+    timerspec.it_interval.tv_nsec = 50000000;
+
+    int timerfd = timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
+    if (timerfd < 0)
+    {
+        perror("timerfd_create");
+        return 1;
+    }
+
+    if (timerfd_settime(timerfd, TFD_TIMER_ABSTIME, &timerspec, NULL) != 0)
+    {
+        perror("timerfd_settime");
+        close(timerfd);
+        return 1;
+    }
+
+    unsigned long count = 0;
+    secs = (long)difftime(countDownDate, nowspec.tv_sec);
+    ssize_t c = 0;
+    int ret = 0;
+    do
+    {
         days = secs / (60 * 60 * 24);
         hours = (secs % (60 * 60 * 24)) / (60 * 60);
         minutes = (secs % (60 * 60)) / 60;
         seconds = secs % 60;
         milliseconds = nowspec.tv_nsec / 1000000;
-        fprintf(stdout, "%03ldd%02ldh%02ldm%02lds%02ld\r", days, hours, minutes, seconds, milliseconds);
+        fprintf(stdout, "%03ldd%02ldh%02ldm%02lds%02ld\r", days, hours,
+                minutes, seconds, milliseconds);
         fflush(stdout);
-        nanosleep(&spec, NULL);
+        c = read(timerfd, &count, sizeof(unsigned long));
+        if (c == -1)
+        {
+            break;
+        }
+        clock_gettime(CLOCK_REALTIME, &nowspec);
+        secs = (long)difftime(countDownDate, nowspec.tv_sec);
+    } while (secs > 0);
+    if (c == -1)
+    {
+        ret = 1;
+        perror("read");
     }
+    close(timerfd);
+    return ret;
+}
+
+int main(void)
+{
+    CentOS_rip();
+    return 0;
 }
