f7a94794c1ce664fe41543451312bf525601cee0
Teruteru <teruterubouzu1024@gmail.com>
2021-12-04 18:02:22 +0900

    Update sandbox

--- src/main2.c
+++ src/main2.c
@@ -1,32 +1,23 @@
 
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    struct tm tm = {0};
-    tm.tm_year = 2021 - 1900;
-    tm.tm_mon = 10;
-    tm.tm_mday = 45;
-    tm.tm_hour = 14;
-    tm.tm_min = 19;
-    tm.tm_sec = 19;
+    time_t machine_time = 0;
+    struct tm machine_tm = { 0 };
+    char buf[BUFSIZ] = "";
+    for (size_t i = 0; i < 2000; i++)
+    {
+        machine_time = time(NULL);
+        localtime_r(&machine_time, &machine_tm);
+        strftime(buf, BUFSIZ, "%Ex %EX", &machine_tm);
+        fprintf(stdout, "%s\n", buf);
+        buf[0] = '\0';
+        sleep(1);
+    }
 
-    time_t time = 0;
-
-    time = mktime(&tm);
-
-    localtime_r(&time, &tm);
-
-    char buf[64];
-    strftime(buf, 64, "%FT%T+09:00", &tm);
-
-    printf("%s\n", buf);
-
-    printf("%lf\n", 0x1.ap-5);
-    // 000111.000
-    //   0001.11000
-    //   1.00011
-    //
     return 0;
 }
