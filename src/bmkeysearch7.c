06270e5e4d24864c87d7a7e784a1ab52235d17e9
Teruteru <teruterubouzu1024@gmail.com>
2021-12-08 22:24:48 +0900

    Remove mdlen

--- src/bmkeysearch7.c
+++ src/bmkeysearch7.c
@@ -52,7 +52,6 @@ int main(int argc, char *argv[])
     const EVP_MD *ripemd160 = EVP_ripemd160();
     EVP_MD_CTX *mdctx = NULL;
     unsigned char hash[EVP_MAX_MD_SIZE] = "";
-    unsigned int mdlen = 0;
     unsigned long tmp = 0;
 
     // counts の定義で _Atomic を使うか reduction を使うかは考えどころ
@@ -68,7 +67,7 @@ int main(int argc, char *argv[])
     size_t jj = 0;
     size_t jj_max = 0;
     unsigned char *sign = NULL;
-#pragma omp parallel for default(none) shared(sha512, ripemd160, memories, stdout) private(hash, mdctx, mdlen, tmp, i, j, ii, ii_max, jj, jj_max, sign) reduction(+: counts[:21])
+#pragma omp parallel for default(none) shared(sha512, ripemd160, memories, stdout) private(hash, mdctx, tmp, i, j, ii, ii_max, jj, jj_max, sign) reduction(+: counts[:21])
     for (i = 0; i < 4362076160UL; i += 1040)
     {
         mdctx = EVP_MD_CTX_new();
@@ -84,17 +83,19 @@ int main(int argc, char *argv[])
                     EVP_DigestInit(mdctx, sha512);
                     EVP_DigestUpdate(mdctx, sign, 65);
                     EVP_DigestUpdate(mdctx, memories + jj, 65);
-                    EVP_DigestFinal(mdctx, hash, &mdlen);
-                    assert(mdlen == 64);
+                    EVP_DigestFinal(mdctx, hash, NULL);
                     EVP_DigestInit(mdctx, ripemd160);
                     EVP_DigestUpdate(mdctx, hash, 64);
-                    EVP_DigestFinal(mdctx, hash, &mdlen);
-                    assert(mdlen == 20);
+                    EVP_DigestFinal(mdctx, hash, NULL);
                     tmp = htobe64(*(unsigned long *)hash);
                     tmp = clzl(tmp);
                     counts[tmp]++;
                     if (tmp >= 4)
                     {
+                        // iiとjjから秘密鍵を読み込む
+                        // 秘密鍵をWIFにエンコードする
+                        // 公開鍵からアドレスにフォーマットする
+                        // あとはBitMessageなりCSVなりご自由に出力する
                         printf("%lu, %zu, %zu\n", tmp, ii, jj);
                         fflush(stdout);
                     }
