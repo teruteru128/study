ae107bd861a79b7cdde108e67d74e885914ef407
Teruteru <teruterubouzu1024@gmail.com>
2022-02-03 21:44:06 +0900

    Update

--- src/totp.c
+++ src/totp.c
@@ -3,37 +3,41 @@
 #include "config.h"
 #endif
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <openssl/hmac.h>
+
+#include <byteswap.h>
 #include <changebase.h>
 #include <math.h>
-#include <byteswap.h>
+#include <openssl/hmac.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-char *generateTOTP(const char *key, time_t time, size_t returnDigits, const EVP_MD *md)
+__wur char *generateTOTP(const char *key, time_t time, size_t returnDigits,
+                         const EVP_MD *md)
 {
-    char timestr[17] = "";
+    char timestr[20] = "";
     unsigned char *key1 = NULL;
     size_t keylength = parseHex(&key1, key);
-    snprintf(timestr, 17, "%016lX", time);
+    snprintf(timestr, 20, "%016lX", time);
     time = (time_t)htobe64((uint64_t)time);
 
     unsigned char mac[EVP_MAX_MD_SIZE + 1];
     unsigned int maclen;
-    HMAC(md, key1, (int)keylength, (void *)&time, 8, mac, &maclen);
+    HMAC_CTX *ctx = HMAC_CTX_new();
+
+    HMAC_Init_ex(ctx, key1, (int)keylength, md, NULL);
+    HMAC_Update(ctx, (unsigned char *)&time, sizeof(time_t));
+    HMAC_Final(ctx, mac, &maclen);
+    HMAC_CTX_free(ctx);
     free(key1);
 
-    unsigned char offset = mac[maclen - 1] & 0xf;
+    size_t offset = (size_t)(mac[maclen - 1] & 0xf);
 
-    size_t binary = (size_t)(((mac[offset] & 0x7f) << 24) |
-                             ((mac[offset + 1] & 0xff) << 16) |
-                             ((mac[offset + 2] & 0xff) << 8) |
-                             ((mac[offset + 3] & 0xff) << 0));
+    size_t binary = (size_t)(htobe32(*(int *)(mac + offset)));
 
     size_t otp = binary % (size_t)pow(10, (double)returnDigits);
 
     char format[10];
-    snprintf(format, 9, "%%0%lulu", returnDigits);
+    snprintf(format, 10, "%%0%lulu", returnDigits);
     char *ret = calloc(returnDigits + 1, sizeof(char));
     // "%0xd"
     snprintf(ret, returnDigits + 1, format, otp);
@@ -52,7 +56,8 @@ int main(int argc, char *argv[])
     const time_t T0 = 0;
     time_t unixtime = time(NULL);
     time_t T = (unixtime - T0) / X;
-    char *totp = generateTOTP_SHA1("3132333435363738393031323334353637383930", T, 6);
+    char key[] = "3132333435363738393031323334353637383930";
+    char *totp = generateTOTP_SHA1(key, T, 6);
     printf("%s\n", totp);
     free(totp);
     return EXIT_SUCCESS;
