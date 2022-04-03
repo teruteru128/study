7b665b7d99f49ff44690b121bf446cc94e9fafda
Teruteru <teruterubouzu1024@gmail.com>
2022-04-03 21:13:13 +0900

    Update

--- src/bmkeysearch11.c
+++ src/bmkeysearch11.c
@@ -12,8 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char a[] = { '0', '1', '2', '3', '4', '5', '6', '7',
-             '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+static const char hextable[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                 '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
 static char *buildTransmitdata(unsigned char *signpub, size_t signpublen,
                                unsigned char *encpub, size_t encpublen)
@@ -36,28 +36,28 @@ static char *buildTransmitdata(unsigned char *signpub, size_t signpublen,
     ret[11] = '1';
     for (size_t i = 0; i < 64; i++)
     {
-        ret[(i << 1) + 12] = a[(signpub[i + 1] >> 4) & 0x0f];
-        ret[(i << 1) + 13] = a[(signpub[i + 1] >> 0) & 0x0f];
+        ret[(i << 1) + 12] = hextable[(signpub[i + 1] >> 4) & 0x0f];
+        ret[(i << 1) + 13] = hextable[(signpub[i + 1] >> 0) & 0x0f];
     }
     for (size_t i = 0; i < 64; i++)
     {
-        ret[(i << 1) + 140] = a[(encpub[i + 1] >> 4) & 0x0f];
-        ret[(i << 1) + 141] = a[(encpub[i + 1] >> 0) & 0x0f];
+        ret[(i << 1) + 140] = hextable[(encpub[i + 1] >> 4) & 0x0f];
+        ret[(i << 1) + 141] = hextable[(encpub[i + 1] >> 0) & 0x0f];
     }
     // 140 + 128
     // FD03E8
     ret[268] = 'f';
     ret[269] = 'd';
     ret[270] = '0';
-    ret[271] = '3';
-    ret[272] = 'e';
-    ret[273] = '8';
+    ret[271] = 'f';
+    ret[272] = 'a';
+    ret[273] = '0';
     ret[274] = 'f';
     ret[275] = 'd';
     ret[276] = '0';
-    ret[277] = '3';
-    ret[278] = 'e';
-    ret[279] = '8';
+    ret[277] = 'f';
+    ret[278] = 'a';
+    ret[279] = '0';
     ret[280] = '\0';
 
     return ret;
@@ -91,12 +91,14 @@ int main(int argc, char const *argv[])
     char *transmitdatahex = NULL;
     int isFirst = 1;
     size_t k = 0;
+    unsigned char *signpubkey = NULL;
     for (i = 0; i < 130; i += 65)
     {
+        signpubkey = publickeys + i;
         for (j = 0; j < 4362076160UL; j += 65)
         {
             EVP_DigestInit(mdctx, sha512);
-            EVP_DigestUpdate(mdctx, publickeys + i, 65);
+            EVP_DigestUpdate(mdctx, signpubkey, 65);
             EVP_DigestUpdate(mdctx, publickeys + j, 65);
             EVP_DigestFinal(mdctx, work, NULL);
             EVP_DigestInit(mdctx, ripemd160);
@@ -106,8 +108,8 @@ int main(int argc, char const *argv[])
             if (work[0] == 0)
             {
                 address = encodeV4Address(work, 20);
-                transmitdatahex = buildTransmitdata(publickeys + i, 65,
-                                                    publickeys + j, 65);
+                transmitdatahex
+                    = buildTransmitdata(signpubkey, 65, publickeys + j, 65);
                 if (isFirst == 1)
                 {
                     isFirst = 0;
@@ -122,10 +124,8 @@ int main(int argc, char const *argv[])
                 free(transmitdatahex);
                 if ((k % 1000) == 999)
                 {
-                    fputs(";\n", stdout);
-                    fputs("INSERT INTO pubkeys(address, addressversion, "
-                          "transmitdata, time, "
-                          "usedpersonally) values\n",
+                    fputs(";\nINSERT INTO pubkeys(address, addressversion, "
+                          "transmitdata, time, usedpersonally) values",
                           stdout);
                     isFirst = 1;
                 }
