090896869693102971e077177bc6f29937e871d6
Teruteru <teruterubouzu1024@gmail.com>
2022-04-17 02:29:45 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -2,14 +2,16 @@
 #define _GNU_SOURCE
 #include "config.h"
 
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <bm.h>
+#include <byteswap.h>
 #include <err.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <locale.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
