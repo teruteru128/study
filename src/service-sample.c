8144f48e58e7970df9bd7b9367e6690e945117ee
Teruteru <teruterubouzu1024@gmail.com>
2022-05-01 22:24:52 +0900

    Update

--- src/service-sample.c
+++ src/service-sample.c
@@ -34,7 +34,7 @@ static volatile int running = 1;
 int main(int argc, char *argv[])
 {
     // socket
-    int s = -1;
+    int serversocket = -1;
 
     struct addrinfo hints;
     struct addrinfo *res = NULL;
@@ -56,22 +56,22 @@ int main(int argc, char *argv[])
     }
     for (ptr = res; ptr != NULL; ptr = ptr->ai_next)
     {
-        s = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-        if (s == -1)
+        serversocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (serversocket == -1)
         {
             continue;
         }
-        if (bind(s, ptr->ai_addr, ptr->ai_addrlen) < 0)
+        if (bind(serversocket, ptr->ai_addr, ptr->ai_addrlen) < 0)
         {
-            close(s);
-            s = -1;
+            close(serversocket);
+            serversocket = -1;
             continue;
         }
-        if (listen(s, SOMAXCONN) < 0)
+        if (listen(serversocket, SOMAXCONN) < 0)
         {
             fprintf(stderr, "listen\n");
-            close(s);
-            s = -1;
+            close(serversocket);
+            serversocket = -1;
             continue;
         }
     }
@@ -79,33 +79,55 @@ int main(int argc, char *argv[])
     uint64_t command = 0;
     struct sockaddr_storage from_sock_addr = { 0 };
     socklen_t addr_len = sizeof(from_sock_addr);
-    int c = accept(s, (struct sockaddr *)&from_sock_addr, &addr_len);
-    if (read(c, &command, sizeof(uint64_t)) < 1)
+    // TODO: マルチスレッド化
+    while (running)
     {
-        close(c);
-        close(s);
-        return EXIT_FAILURE;
-    }
-    uint64_t length = 0;
-    if (read(length, &command, sizeof(uint64_t)) < 1)
-    {
-        close(c);
-        close(s);
-        return EXIT_FAILURE;
-    }
-    unsigned char buf[BUFSIZ] = "";
-    uint64_t i = length;
-    ssize_t size = 0;
-    while (i < length)
-    {
-        size = write(c, buf, BUFSIZ);
-        if (size < 0)
+        int connection = accept(serversocket, (struct sockaddr *)&from_sock_addr, &addr_len);
+        if (connection == -1)
         {
-            break;
+            perror("accept");
+            continue;
         }
-        i += size;
+        if (read(connection, &command, sizeof(uint64_t)) < 1)
+        {
+            close(connection);
+            continue;
+        }
+        uint64_t length = 0;
+        if (read(length, &command, sizeof(uint64_t)) < 1)
+        {
+            close(connection);
+            continue;
+        }
+        unsigned char buf[BUFSIZ] = "";
+        uint64_t i = length;
+        ssize_t size = 0;
+        while (i > 0)
+        {
+            if (i >= BUFSIZ)
+            {
+                // データ送信中に割り込み食らったら接続切れるんかな？
+                size = write(connection, buf, BUFSIZ);
+                if (size < 0)
+                {
+                    perror("");
+                    break;
+                }
+                i -= size;
+            }
+            else
+            {
+                size = write(connection, buf, i);
+                if (size < 0)
+                {
+                    perror("");
+                    break;
+                }
+                i = 0;
+            }
+        }
+        close(connection);
     }
-    close(c);
-    close(s);
+    close(serversocket);
     return EXIT_SUCCESS;
 }
