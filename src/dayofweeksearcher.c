0f014d45aaa315a447105ad228381f7d1c4afbdf
Teruteru <teruterubouzu1024@gmail.com>
2022-05-12 17:05:20 +0900

    Update dayofweeksearcher

--- src/dayofweeksearcher.c
+++ src/dayofweeksearcher.c
@@ -7,14 +7,29 @@ int main(int argc, char const *argv[])
     struct tm tm = { 0 };
     tm.tm_year = 2022 - 1900;
     tm.tm_mon = 1;
-    tm.tm_mday = 22;
+    tm.tm_mday = 13;
     time_t a = 0;
-    a = mktime(&tm);
-    while (localtime_r(&a, &tm)->tm_wday != 1)
+    for (int count = 0; count < 3; count++)
     {
-        tm.tm_year++;
         a = mktime(&tm);
+        while (localtime_r(&a, &tm)->tm_wday != 5)
+        {
+            tm.tm_mon++;
+            if (tm.tm_mon >= 12)
+            {
+                tm.tm_mon = 0;
+                tm.tm_year++;
+            }
+            a = mktime(&tm);
+        }
+        printf("%d/%d/%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+        tm.tm_mon++;
+        if (tm.tm_mon >= 12)
+        {
+            tm.tm_mon = 0;
+            tm.tm_year++;
+        }
     }
-    printf("%d/%d/%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+
     return 0;
 }
