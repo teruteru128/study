9e60312fafae6c44218bc8fd4f498f73eb2159b6
Teruteru <teruterubouzu1024@gmail.com>
2022-07-16 22:29:20 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -4,58 +4,32 @@
 
 #include <complex.h>
 #include <math.h>
+#include <limits.h>
 #include <stdio.h>
 #include <sys/random.h>
 
 int main(int argc, char const *argv[])
 {
-    unsigned char buf[96];
-
-    for (size_t i = 0; i < 96; i++)
+    // 2の累乗の中で最も10の累乗に近いのは？
+    // n*log(2)/log(10)
+    long double a = 10;
+    long double b, c;
+    long double min = 1;
+    long double max = 0;
+    for (long i = 1; i <= 20000; i++)
     {
-        printf("%02x", buf[i]);
+        a = i * log10l(2);
+        b = modfl(a, &c);
+        if (b < min)
+        {
+            min = b;
+            printf("min: %15ld, %.20Lf, %6ld\n", i, b, (long)c);
+        }
+        if (b > max)
+        {
+            max = b;
+            printf("max: %15ld, %.20Lf, %6ld\n", i, b, (long)c);
+        }
     }
-    fputs("\n", stdout);
-
-    ssize_t len = getrandom(buf, 96, 0);
-    if (len < 0)
-    {
-        return 1;
-    }
-    for (size_t i = 0; i < 96; i++)
-    {
-        printf("%02x", buf[i]);
-    }
-    fputs("\n", stdout);
-    len = getrandom(buf, 96, GRND_NONBLOCK);
-    if (len < 0)
-    {
-        return 1;
-    }
-    for (size_t i = 0; i < 96; i++)
-    {
-        printf("%02x", buf[i]);
-    }
-    fputs("\n", stdout);
-    len = getrandom(buf, 96, GRND_RANDOM);
-    if (len < 0)
-    {
-        return 1;
-    }
-    for (size_t i = 0; i < 96; i++)
-    {
-        printf("%02x", buf[i]);
-    }
-    fputs("\n", stdout);
-    len = getrandom(buf, 96, GRND_INSECURE);
-    if (len < 0)
-    {
-        return 1;
-    }
-    for (size_t i = 0; i < 96; i++)
-    {
-        printf("%02x", buf[i]);
-    }
-    fputs("\n", stdout);
     return 0;
 }
