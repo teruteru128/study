e3c9932add95423595ae0d7a8753a8edee49f838
Teruteru <teruterubouzu1024@gmail.com>
2022-07-16 16:48:26 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -2,6 +2,60 @@
 #define _GNU_SOURCE
 #include "config.h"
 
+#include <complex.h>
+#include <math.h>
 #include <stdio.h>
+#include <sys/random.h>
 
-int main(int argc, char const *argv[]) { return 0; }
+int main(int argc, char const *argv[])
+{
+    unsigned char buf[96];
+
+    for (size_t i = 0; i < 96; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    fputs("\n", stdout);
+
+    ssize_t len = getrandom(buf, 96, 0);
+    if (len < 0)
+    {
+        return 1;
+    }
+    for (size_t i = 0; i < 96; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    fputs("\n", stdout);
+    len = getrandom(buf, 96, GRND_NONBLOCK);
+    if (len < 0)
+    {
+        return 1;
+    }
+    for (size_t i = 0; i < 96; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    fputs("\n", stdout);
+    len = getrandom(buf, 96, GRND_RANDOM);
+    if (len < 0)
+    {
+        return 1;
+    }
+    for (size_t i = 0; i < 96; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    fputs("\n", stdout);
+    len = getrandom(buf, 96, GRND_INSECURE);
+    if (len < 0)
+    {
+        return 1;
+    }
+    for (size_t i = 0; i < 96; i++)
+    {
+        printf("%02x", buf[i]);
+    }
+    fputs("\n", stdout);
+    return 0;
+}
