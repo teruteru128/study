7e116b9b126e698bb949fe15ad6acb53d9dda0ac
Teruteru <teruterubouzu1024@gmail.com>
2022-07-23 20:38:11 +0900

    remove executable main2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-int main(int, char **)
+extern "C"
+{
+    int hiho(int, char **);
+}
+
+int main(int argc, char **argv)
 {
     std::cout << "Hello, world!";
     std::cout << std::endl;
+    return hiho(argc, argv);
 }
--- src/main2.c
+++ src/main2.c
@@ -4,4 +4,11 @@
 
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) { return 0; }
+int hiho(int argc, char **argv)
+{
+    if (argc > 1)
+        printf("%c\n", argv[1][0] + 8);
+    else
+        printf("%c\n", 'G' + 8);
+    return 0;
+}
