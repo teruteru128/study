f67fe1be19dff722fbb8e68a23d716c77ece26fa
Teruteru <teruterubouzu1024@gmail.com>
2022-07-24 13:43:32 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -7,7 +7,7 @@
 int hiho(int argc, char **argv)
 {
     if (argc > 1)
-        printf("%c\n", argv[1][0] + 8);
+        printf("%c\n", (argv[1][0] + 8) & 0x7f);
     else
         printf("%c\n", 'G' + 8);
     return 0;
