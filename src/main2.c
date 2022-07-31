ad7632f71adcbb2c17e76d6d034ca347a9b2aa58
Teruteru <teruterubouzu1024@gmail.com>
2022-07-31 21:00:31 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -2,28 +2,40 @@
 #define _GNU_SOURCE
 #include "config.h"
 
-#include <complex.h>
-#include <ctype.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/random.h>
 
 int hiho(int argc, char **argv, const char **envp)
 {
-    size_t count = 0;
-    // under bust 65cm
-    double ub = 62.0;
-    char cup[12] = "";
-    for (size_t offset = 0; offset < 11; offset++)
+    const size_t size = 32UL;
+    const size_t num = 16777216UL;
+    const size_t length = size * num;
+    unsigned char *buf = malloc(length);
+    ssize_t ge = 0;
+    char filename[PATH_MAX] = "";
+    FILE *fout = NULL;
+    for (size_t i = 8; i < 256; i++)
     {
-        for (size_t alphabet = 0; alphabet < 26; alphabet++, count++)
+        printf("(%zu)乱数取得中\n", i);
+        ge = getrandom(buf, length, GRND_RANDOM);
+        printf("(%zu)乱数取得完了\n", i);
+        snprintf(filename, PATH_MAX, "privateKeys%zu.bin", i);
+        fout = fopen(filename, "wb");
+        if (fout == NULL)
         {
-            cup[offset] = 'A' + alphabet;
-            printf("%s, %zu, %6.1lfcm\n", cup, count, ub + 10 + 2.5 * count);
+            perror("fopen wb");
+            break;
         }
+        fwrite(buf, size, num, fout);
+        fclose(fout);
+        printf("(%zu)乱数ファイル書き込み完了\n", i);
+        fout = NULL;
     }
 
-    printf("%zu\n", sizeof(float complex));
+    memset(buf, 0, length);
+    free(buf);
     return 0;
 }
