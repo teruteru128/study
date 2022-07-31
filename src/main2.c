7736baa32086571496f8e2c723d2c04b97bdfebd
Teruteru <teruterubouzu1024@gmail.com>
2022-07-31 14:19:55 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -2,66 +2,28 @@
 #define _GNU_SOURCE
 #include "config.h"
 
+#include <complex.h>
 #include <ctype.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const char *hex2bin(const char *hex, unsigned char *bin, size_t binsize)
-{
-    if (hex && bin)
-    {
-        for (; binsize && isxdigit(hex[0]) && isxdigit(hex[1]);
-             hex += 2, bin += 1, binsize -= 1)
-        { //  fujitanozomu さんの指摘を適用
-            int r = sscanf(hex, "%2hhx", bin); //  shiracamus さんの指摘を適用
-            if (r != 1)
-            {
-                break;
-            }
-        }
-    }
-
-    return hex;
-}
-
 int hiho(int argc, char **argv, const char **envp)
 {
-    size_t lines = 0;
-    FILE *fin = NULL;
-    char pathname[PATH_MAX];
-    unsigned char pubkeybuf[65] = { 0 };
-    hex2bin(
-        "0400000063ca291bb91c80a6141de191592c8f5a50b522b523fbb1cdf5814ea586dfc"
-        "6aa167418f1eea92621f4f4544c591f9ee8334b7ac1dcc27917f1edf993aa",
-        pubkeybuf, 65);
-
-    unsigned char *buf = malloc(16777216 * 65);
-    unsigned char *catch = NULL;
-    for (size_t i = 0; i < 8; i++)
+    size_t count = 0;
+    // under bust 65cm
+    double ub = 62.0;
+    char cup[12] = "";
+    for (size_t offset = 0; offset < 11; offset++)
     {
-        snprintf(pathname, PATH_MAX, "publicKeys%zu.bin", i);
-        fin = fopen(pathname, "rb");
-        if (fin == NULL)
+        for (size_t alphabet = 0; alphabet < 26; alphabet++, count++)
         {
-            perror("fopen");
-            break;
+            cup[offset] = 'A' + alphabet;
+            printf("%s, %zu, %6.1lfcm\n", cup, count, ub + 10 + 2.5 * count);
         }
-        if (fread(buf, 65, 16777216, fin) != 16777216)
-        {
-            perror("fread");
-            fclose(fin);
-            exit(EXIT_FAILURE);
-        }
-        if ((catch = memmem(buf, 16777216 * 65, pubkeybuf, 65)) != NULL)
-        {
-            printf("%s, %ld\n", pathname, (catch - buf) / 65);
-        }
-        fclose(fin);
     }
-    memset(buf, 0, 16777216 * 65);
-    free(buf);
 
+    printf("%zu\n", sizeof(float complex));
     return 0;
 }
