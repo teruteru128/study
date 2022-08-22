16efa707e93c567eb913417ebd2789915460bf77
Teruteru <teruterubouzu1024@gmail.com>
2022-08-22 16:28:54 +0900

    Refactor editpngphys

--- src/editpngphys.c
+++ src/editpngphys.c
@@ -1,8 +1,10 @@
 
 #include <inttypes.h>
+#include <math.h>
 #include <png.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 // 画像ヘッダ
 struct IHDR
@@ -23,10 +25,13 @@ struct pHYs
     int type;
 };
 
-static png_byte **row_pointers = NULL;
-
-static int read_png(const char *inpath, struct IHDR *ihdr, struct pHYs *phys)
+static int read_png(const char *inpath, struct IHDR *ihdr, struct pHYs *phys,
+                    png_byte ***row_pointers)
 {
+    if (inpath == NULL || row_pointers == NULL)
+    {
+        return 1;
+    }
     FILE *infp = fopen(inpath, "rb");
     if (infp == NULL)
     {
@@ -46,12 +51,6 @@ static int read_png(const char *inpath, struct IHDR *ihdr, struct pHYs *phys)
         return (EXIT_FAILURE);
     }
 
-    png_info *end_info = png_create_info_struct(png_ptr);
-    if (!end_info)
-    {
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-        return (EXIT_FAILURE);
-    }
     /* Set error handling if you are using the setjmp/longjmp method (this
      * is the normal method of doing things with libpng).  REQUIRED unless
      * you set up your own error handlers in the png_create_read_struct()
@@ -71,89 +70,96 @@ static int read_png(const char *inpath, struct IHDR *ihdr, struct pHYs *phys)
 
     png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
 
-    if (png_get_IHDR(png_ptr, info_ptr, &ihdr->width, &ihdr->height,
-                     &ihdr->bit_depth, &ihdr->color_type,
-                     &ihdr->interlace_method, &ihdr->compression_method,
-                     &ihdr->filter_method))
+    if (ihdr)
     {
-        printf("image header: %" PRId32 " %" PRId32 "\n", ihdr->width,
-               ihdr->height);
-        printf("%d %d %d %d %d\n", ihdr->bit_depth, ihdr->color_type,
-               ihdr->interlace_method, ihdr->compression_method,
-               ihdr->filter_method);
+        if (png_get_IHDR(png_ptr, info_ptr, &ihdr->width, &ihdr->height,
+                         &ihdr->bit_depth, &ihdr->color_type,
+                         &ihdr->interlace_method, &ihdr->compression_method,
+                         &ihdr->filter_method))
+        {
+            printf("image header: %" PRId32 " %" PRId32 "\n", ihdr->width,
+                   ihdr->height);
+            printf("%d %d %d %d %d\n", ihdr->bit_depth, ihdr->color_type,
+                   ihdr->interlace_method, ihdr->compression_method,
+                   ihdr->filter_method);
+        }
+        else
+        {
+            printf("png_get_IHDR failed\n");
+        }
     }
-    else
+    if (phys)
     {
-        printf("png_get_IHDR failed\n");
-    }
-    if (png_get_pHYs(png_ptr, info_ptr, &phys->res_x, &phys->res_y,
-                     &phys->type))
-    {
-        printf("physical pixel resolution: %" PRId32 " %" PRId32 " %d\n",
-               phys->res_x, phys->res_y, phys->type);
-    }
-    else
-    {
-        printf("png_get_pHYs failed\n");
+        if (png_get_pHYs(png_ptr, info_ptr, &phys->res_x, &phys->res_y,
+                         &phys->type))
+        {
+            printf("physical pixel resolution: %" PRId32 " %" PRId32 " %d\n",
+                   phys->res_x, phys->res_y, phys->type);
+        }
+        else
+        {
+            printf("png_get_pHYs failed\n");
+        }
     }
 
     // dupliacte rows
     png_byte **original_row_pointers = png_get_rows(png_ptr, info_ptr);
-    row_pointers = malloc(sizeof(png_byte *) * ihdr->height);
+    *row_pointers = malloc(sizeof(png_byte *) * ihdr->height);
     size_t rowsize = png_get_rowbytes(png_ptr, info_ptr);
     for (size_t y = 0; y < ihdr->height; y++)
     {
-        row_pointers[y] = malloc(rowsize);
-        memcpy(row_pointers[y], original_row_pointers[y], rowsize);
+        (*row_pointers)[y] = malloc(rowsize);
+        memcpy((*row_pointers)[y], original_row_pointers[y], rowsize);
     }
 
-    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     fclose(infp);
     return 0;
 }
 
-static int write_png(const char *outpath, struct IHDR *ihdr, struct pHYs *phys)
+static int write_png(const char *outpath, struct IHDR *ihdr, struct pHYs *phys,
+                     png_byte **row_pointers)
 {
-    FILE *outfp = fopen(outpath, "wb");
-    png_struct *outpng_ptr
+    FILE *fp = fopen(outpath, "wb");
+    png_struct *png_ptr
         = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (outpng_ptr == NULL)
+    if (png_ptr == NULL)
     {
-        printf("outpng_ptr is null\n");
-        fclose(outfp);
+        printf("png_ptr is null\n");
+        fclose(fp);
         return EXIT_FAILURE;
     }
-    png_info *outinfo_ptr = png_create_info_struct(outpng_ptr);
-    if (outinfo_ptr == NULL)
+    png_info *info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL)
     {
-        printf("outinfo_ptr is null\n");
-        png_destroy_write_struct(&outpng_ptr, NULL);
-        fclose(outfp);
+        printf("info_ptr is null\n");
+        png_destroy_write_struct(&png_ptr, NULL);
+        fclose(fp);
         return EXIT_FAILURE;
     }
-    if (setjmp(png_jmpbuf(outpng_ptr)))
+    if (setjmp(png_jmpbuf(png_ptr)))
     {
         /* Free all of the memory associated with the png_ptr and info_ptr.
          */
-        png_destroy_read_struct(&outpng_ptr, &outinfo_ptr, NULL);
-        fclose(outfp);
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        fclose(fp);
         /* If we get here, we had a problem reading the file. */
         return (EXIT_FAILURE);
     }
-    png_init_io(outpng_ptr, outfp);
+    png_init_io(png_ptr, fp);
     /*
      * PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE,
      * PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT
      */
-    png_set_IHDR(outpng_ptr, outinfo_ptr, ihdr->width, ihdr->height,
+    png_set_IHDR(png_ptr, info_ptr, ihdr->width, ihdr->height,
                  ihdr->bit_depth, ihdr->color_type, ihdr->interlace_method,
                  ihdr->compression_method, ihdr->filter_method);
-    png_set_pHYs(outpng_ptr, outinfo_ptr, phys->res_x, phys->res_y,
+    png_set_pHYs(png_ptr, info_ptr, phys->res_x, phys->res_y,
                  phys->type);
-    png_set_rows(outpng_ptr, outinfo_ptr, row_pointers);
-    png_write_png(outpng_ptr, outinfo_ptr, 0, NULL);
-    png_destroy_write_struct(&outpng_ptr, &outinfo_ptr);
-    fclose(outfp);
+    png_set_rows(png_ptr, info_ptr, row_pointers);
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    fclose(fp);
     return 0;
 }
 
@@ -165,15 +171,16 @@ int main(int argc, char const *argv[], const char **envp)
 {
     struct IHDR ihdr = { 0 };
     struct pHYs phys = { 0 };
-    const char inpath[]
-        = "/mnt/g/iandm/image/waifu2x/pixiv.net/"
-          "32668232_p0(UpRGB)(noise_scale)(Level0)(x8.000000).png";
-    const char outpath[] = "32668232_p0_350dpi.png";
-    read_png(inpath, &ihdr, &phys);
+    const char inpath[] = "/mnt/g/iandm/image/waifu2x/pixiv.net/"
+                          "52629088_p0(RGB)(scale)(x2.000000)(16bit).png";
+    const char outpath[]
+        = "/mnt/g/iandm/image/waifu2x/pixiv.net/52629088_p0_350dpi.png";
+    png_byte **row_pointers = NULL;
+    read_png(inpath, &ihdr, &phys, &row_pointers);
     printf("read ok\n");
     // 350 dpi to dots per meter
     phys.res_x = phys.res_y = floor((350 * 10000) / 254.);
-    write_png(outpath, &ihdr, &phys);
+    write_png(outpath, &ihdr, &phys, row_pointers);
     for (size_t y = 0; y < ihdr.height; y++)
     {
         free(row_pointers[y]);
