4ad1f312e35398eb34ff5e4f67a7f1e0a512039d
Teruteru <teruterubouzu1024@gmail.com>
2022-08-25 08:17:00 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -1,6 +1,7 @@
 
 #define _GNU_SOURCE
 
+#include <inttypes.h>
 #include <openssl/err.h>
 #include <openssl/evp.h>
 #include <setjmp.h>
@@ -33,46 +34,26 @@
  */
 int hiho(int argc, char **argv, const char **envp)
 {
-    EVP_PKEY_CTX *fromctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
-    OSSL_PARAM_BLD *param_bld = OSSL_PARAM_BLD_new();
-    OSSL_PARAM *params = NULL;
-    // :thinking:
-    unsigned char pub_data[]
-        = { 0x00, 0x88, 0xf7, 0x5b, 0x93, 0x99, 0xfe, 0x90, 0xff, 0xf0, 0x69,
-            0x2e, 0x63, 0x5b, 0xf6, 0xc9, 0xcb, 0x35, 0x74, 0x70, 0x3b, 0x5f,
-            0x16, 0xab, 0xcd, 0x39, 0xd0, 0xad, 0xba, 0xc6, 0x5c, 0x01, 0xbb,
-            0x98, 0x87, 0x24, 0x93, 0xbb, 0x37, 0xf2, 0x44, 0x31, 0x23, 0xbd,
-            0x65, 0x76, 0x61, 0x79, 0x56, 0x8e, 0x8b, 0xc3, 0xe7, 0xe7, 0x28,
-            0xf1, 0x66, 0x90, 0xb9, 0x1f, 0x05, 0x26, 0x8c, 0xb1, 0x8b };
-
-    OSSL_PARAM_BLD_push_utf8_string(param_bld, OSSL_PKEY_PARAM_GROUP_NAME,
-                                    "secp256k1", 0);
-    OSSL_PARAM_BLD_push_octet_string(param_bld, OSSL_PKEY_PARAM_PUB_KEY,
-                                     pub_data, sizeof(pub_data));
-    params = OSSL_PARAM_BLD_to_param(param_bld);
-    if (params == NULL)
+    uint64_t a = 0;
+    uint64_t b = 0;
+    uint64_t b_max = 0;
+    uint64_t c = 0x44088125286DL ^ 0x5DEECE66DL;
+    b = ((c * 0x5DEECE66DL) + 0xb) & 0xFFFFFFFFFFFFL;
+    a = ((b * 0x5DEECE66DL) + 0xb) & 0xFFFFFFFFFFFFL;
+    printf("0x%016" PRIx64 "->0x%016" PRIx64 "->0x%016" PRIx64 "\n", c ^ 0x5DEECE66DL, b, a);
+    a = 0xffffff000000L;
+    for (; a < 0x1000000000000L; a++)
     {
-        printf("params is null\n");
-        return 1;
+        b = ((a - 0xb) * 0xDFE05BCB1365L) & 0xFFFFFFFFFFFFL;
+        c = ((b - 0xb) * 0xDFE05BCB1365L) & 0xFFFFFFFFFFFFL;
+        if ((b & 0xffffff800000L) == 0xffffff800000L)
+        {
+            printf("0x%016" PRIx64 "->0x%016" PRIx64 "->0x%016" PRIx64 "\n", c ^ 0x5DEECE66DL, b, a);
+            b_max = b;
+        }
     }
-    EVP_PKEY_fromdata_init(fromctx);
-    EVP_PKEY *pkey = NULL;
-    if (EVP_PKEY_fromdata(fromctx, &pkey, EVP_PKEY_PUBLIC_KEY, params) != 1)
-    {
-        fprintf(stderr, "err EVP_PKEY_fromdata %s\n", ERR_reason_error_string(ERR_get_error()));
-        return 1;
-    }
-    EVP_PKEY_CTX_free(fromctx);
-
-    EVP_PKEY_CTX *checkctx = EVP_PKEY_CTX_new_from_pkey(NULL, pkey, NULL);
-    if (checkctx == NULL)
-    {
-        printf("err EVP_PKEY_CTX_new_from_pkey\n");
-        return 1;
-    }
-    int ret = EVP_PKEY_public_check(checkctx);
-    printf("%d\n", ret);
-    EVP_PKEY_CTX_free(checkctx);
-
+    // ↓2回連続getFloatで-1が出るseed 2つ
+    // 125352706827826
+    // 116229385253865
     return 0;
 }
