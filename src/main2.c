9602df7f2c6584b9e0e8f8735b58efd9064d4cd5
Teruteru <teruterubouzu1024@gmail.com>
2022-10-11 15:35:27 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -1,5 +1,5 @@
 
-#define _GNU_SOURCE
+#define _GNU_SOURCE 1
 #define OPENSSL_API_COMPAT 0x30000000L
 #define OPENSSL_NO_DEPRECATED 1
 
@@ -58,5 +58,60 @@
  */
 int hiho(int argc, char **argv, const char **envp)
 {
+    int i, ret;
+    struct gaicb *reqs[argc - 1];
+    char host[NI_MAXHOST];
+    struct addrinfo *res;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s HOST...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < argc - 1; i++)
+    {
+        reqs[i] = malloc(sizeof(*reqs[0]));
+        if (reqs[i] == NULL)
+        {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        memset(reqs[i], 0, sizeof(*reqs[0]));
+        reqs[i]->ar_name = argv[i + 1];
+    }
+
+    puts("ma!");
+    ret = getaddrinfo_a(GAI_WAIT, reqs, argc - 1, NULL);
+    if (ret != 0)
+    {
+        fprintf(stderr, "getaddrinfo_a() failed: %s\n", gai_strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    puts("ma!!");
+
+    for (i = 0; i < argc - 1; i++)
+    {
+        printf("%s: ", reqs[i]->ar_name);
+        ret = gai_error(reqs[i]);
+        if (ret == 0)
+        {
+            res = reqs[i]->ar_result;
+
+            ret = getnameinfo(res->ai_addr, res->ai_addrlen, host,
+                              sizeof(host), NULL, 0, NI_NUMERICHOST);
+            if (ret != 0)
+            {
+                fprintf(stderr, "getnameinfo() failed: %s\n",
+                        gai_strerror(ret));
+                exit(EXIT_FAILURE);
+            }
+            puts(host);
+        }
+        else
+        {
+            puts(gai_strerror(ret));
+        }
+    }
     return 0;
 }
