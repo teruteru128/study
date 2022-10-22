5bc178369c48974dbc8bbaf100d9256859246d52
Teruteru <teruterubouzu1024@gmail.com>
2022-10-22 17:40:18 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -55,7 +55,8 @@
 
 #define SECKEY "ioxhJc1lIE2m+WFdBg3ieQb6rk8sSvg3wRv/ImJz2tc="
 
-/*
+/**
+ * @brief 
  * 秘密鍵かな？
  * ioxhJc1lIE2m+WFdBg3ieQb6rk8sSvg3wRv/ImJz2tc=
  * cm2E2vmE0Nd8aVP/4Ph2S1R6C5bkC1H7CiUBzbcDG3U=
@@ -83,9 +84,20 @@
  * 丸数字の1から50までforで出す
  * timer_create+sigeventでタイマーを使ってスレッドを起動する
  * decodable random source?
+ * 
+ * @param argc 
+ * @param argv 
+ * @param envp 
+ * @return int 
  */
 int hiho(int argc, char **argv, const char **envp)
 {
+    if (argc < 2)
+    {
+        fprintf(stderr, "出力ファイル\n");
+        return 1;
+    }
+    /*
     BIO *mem = BIO_new_mem_buf(SECKEY, strlen(SECKEY));
     BIO *base64 = BIO_new(BIO_f_base64());
     if (base64 == NULL || mem == NULL)
@@ -118,7 +130,7 @@ int hiho(int argc, char **argv, const char **envp)
     }
     printf("\n");
     BIO_free_all(io);
-
+    */
     png_color c1 = { 0 };
     c1.red = 249;
     c1.green = 0;
@@ -128,32 +140,42 @@ int hiho(int argc, char **argv, const char **envp)
     c2.green = 147;
     c2.blue = 236;
     struct IHDR ihdr = { 0 };
-    ihdr.width = 1920;
-    ihdr.height = 1080;
+    ihdr.width = 1920 * 4;
+    ihdr.height = 1080 * 4;
     ihdr.bit_depth = 8;
-    ihdr.color_type = PNG_COLOR_TYPE_RGB_ALPHA;
+    ihdr.color_type = PNG_COLOR_TYPE_RGB;
     ihdr.interlace_method = PNG_INTERLACE_NONE;
     ihdr.compression_method = PNG_COMPRESSION_TYPE_DEFAULT;
     ihdr.filter_method = PNG_NO_FILTERS;
-    const double h = hypot(1920, 1080);
-    png_byte **data = malloc(sizeof(png_byte *) * 1080);
+    png_byte **data = malloc(sizeof(png_byte *) * ihdr.height);
     ssize_t len2 = 0;
-    for (size_t y = 0; y < 1080; y++)
+    unsigned char colors[6];
+    ssize_t a = getrandom(colors, 6, 0);
+    if (a < 6)
     {
-        data[y] = malloc(sizeof(png_byte) * 1920 * 4);
-        for (size_t x = 0; x < 7680; x += 4)
+        return 1;
+    }
+    size_t rawwidthsize = sizeof(png_byte) * ihdr.width * 3;
+    for (size_t y = 0; y < ihdr.height; y++)
+    {
+        data[y] = malloc(rawwidthsize);
+        for (size_t x = 0; x < rawwidthsize; x += 3)
         {
-            data[y][x + 0] = (png_byte)((255. * (1080 - y) + 249 * y) / 1080);
-            data[y][x + 1] = (png_byte)((147. * (1080 - y) + 0 * y) / 1080);
-            data[y][x + 2] = (png_byte)((236. * (1080 - y) + 149 * y) / 1080);
-            data[y][x + 3] = (png_byte)((255. * (h - hypot(1080 - y, x/4))) / h);
+            data[y][x + 0]
+                = (png_byte)(((double)colors[0] * (ihdr.height - y) + colors[3] * y)
+                             / ihdr.height);
+            data[y][x + 1]
+                = (png_byte)(((double)colors[1] * (ihdr.height - y) + colors[4] * y)
+                             / ihdr.height);
+            data[y][x + 2]
+                = (png_byte)(((double)colors[2] * (ihdr.height - y) + colors[5] * y)
+                             / ihdr.height);
+            // data[y][x + 3] = (png_byte)(255. * (ihdr.width - (x / 4)) / ihdr.width);
         }
     }
-    write_png("/mnt/c/Users/terut/OneDrive/Pictures/projects/study/gradation/"
-              "gradation12.png",
-              &ihdr, NULL, NULL, 0, data);
+    write_png(argv[1], &ihdr, NULL, NULL, 0, data);
 error:
-    for (size_t y = 0; y < 1080; y++)
+    for (size_t y = 0; y < ihdr.height; y++)
     {
         free(data[y]);
     }
