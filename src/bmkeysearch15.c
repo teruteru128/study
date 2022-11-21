4b2eb7a6ca87241557b44ff01bcdd4fdcd6fef85
Teruteru <teruterubouzu1024@gmail.com>
2022-11-21 22:55:59 +0900

    Support old openssl

--- src/bmkeysearch15.c
+++ src/bmkeysearch15.c
@@ -129,8 +129,13 @@ static int dappunda(const EVP_MD *sha512, const EVP_MD *ripemd160)
         }
         EVP_MD_CTX *shactx2 = EVP_MD_CTX_new();
         EVP_MD_CTX *ripectx = EVP_MD_CTX_new();
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
         EVP_DigestInit_ex2(shactx2, sha512, NULL);
         EVP_DigestInit_ex2(ripectx, ripemd160, NULL);
+#else
+        EVP_DigestInit_ex(shactx2, sha512, NULL);
+        EVP_DigestInit_ex(ripectx, ripemd160, NULL);
+#endif
         unsigned char encbuf[ENC_CACHE_SIZE * PUBLIC_KEY_LENGTH];
         size_t sigindex = 0;
         size_t encindex = 0;
@@ -167,7 +172,11 @@ static int dappunda(const EVP_MD *sha512, const EVP_MD *ripemd160)
             clock_gettime(CLOCK_MONOTONIC, &startspec);
             for (sigindex = 0; sigindex < CTX_CACHE_SIZE; sigindex++)
             {
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
                 EVP_DigestInit_ex2(shactx1[sigindex], sha512, NULL);
+#else
+                EVP_DigestInit_ex(shactx1[sigindex], sha512, NULL);
+#endif
                 EVP_DigestUpdate(shactx1[sigindex],
                                  (PublicKey *)publicKeyGlobal + sigglobalindex
                                      + sigindex,
@@ -189,7 +198,11 @@ static int dappunda(const EVP_MD *sha512, const EVP_MD *ripemd160)
                         EVP_DigestFinal_ex(shactx2, hash, NULL);
                         // init_ex2とcopy_exってどっちが早いんやろうな？
                         // 全部コピーするからcopy_exのほうが遅いのかもしれん
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
                         EVP_DigestInit_ex2(ripectx, ripemd160, NULL);
+#else
+                        EVP_DigestInit_ex(ripectx, ripemd160, NULL);
+#endif
                         EVP_DigestUpdate(ripectx, hash, 64);
                         EVP_DigestFinal_ex(ripectx, hash, NULL);
                         // GPUで計算するときはハッシュだけGPUで計算して
@@ -259,10 +272,15 @@ int compar(const void *a, const void *b, void *arg)
 int searchAddressFromExistingKeys3()
 {
     fprintf(stderr, "my pid is %d\n", getpid());
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     OSSL_PROVIDER *legacy = OSSL_PROVIDER_load(NULL, "legacy");
     OSSL_PROVIDER *def = OSSL_PROVIDER_load(NULL, "default");
     EVP_MD *sha512 = EVP_MD_fetch(NULL, "sha512", NULL);
     EVP_MD *ripemd160 = EVP_MD_fetch(NULL, "ripemd160", NULL);
+#else
+    const EVP_MD *sha512 = EVP_sha512();
+    const EVP_MD *ripemd160 = EVP_ripemd160();
+#endif
     struct sigaction action = { 0 };
     struct sigaction oact = { 0 };
     action.sa_flags = SA_SIGINFO;
@@ -277,10 +295,12 @@ int searchAddressFromExistingKeys3()
     {
         dappunda(sha512, ripemd160);
     }
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
     EVP_MD_free(sha512);
     EVP_MD_free(ripemd160);
     OSSL_PROVIDER_unload(def);
     OSSL_PROVIDER_unload(legacy);
+#endif
     return ret;
 }
 
