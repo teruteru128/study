db66f60ad50e64706d3582970f6578078a4c232a
Teruteru <teruterubouzu1024@gmail.com>
2022-11-21 23:04:08 +0900

    Support old openssl

--- src/searchAddressFromExistingKeys.c
+++ src/searchAddressFromExistingKeys.c
@@ -77,8 +77,6 @@ int searchAddressFromExistingKeys(int argc, char **argv)
     // 16件ずつローカル変数にコピーするのって面倒じゃない……？
     unsigned char a[1040];
     unsigned char b[1040];
-    OSSL_PROVIDER *legacy = OSSL_PROVIDER_load(NULL, "legacy");
-    OSSL_PROVIDER *def = OSSL_PROVIDER_load(NULL, "default");
     EVP_MD_CTX *ctx0 = EVP_MD_CTX_new();
     EVP_MD_CTX *ctx1 = EVP_MD_CTX_new();
     EVP_MD_CTX *ctx2 = EVP_MD_CTX_new();
@@ -87,21 +85,23 @@ int searchAddressFromExistingKeys(int argc, char **argv)
     size_t i;
     size_t j;
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    OSSL_PROVIDER *legacy = OSSL_PROVIDER_load(NULL, "legacy");
+    OSSL_PROVIDER *def = OSSL_PROVIDER_load(NULL, "default");
     EVP_MD *sha512 = EVP_MD_fetch(NULL, "sha512", NULL);
-    EVP_MD *ripemd160 = EVP_MD_fetch(NULL, "ripemd160", NULL);
     EVP_DigestInit_ex2(ctx1, sha512, NULL);
-#else
-    const EVP_MD *sha512 = EVP_sha512();
-    EVP_DigestInit_ex(ctx1, sha512, NULL);
-    const EVP_MD *ripemd160 = EVP_ripemd160();
-#endif
-    unsigned char hash[EVP_MAX_MD_SIZE];
+    EVP_MD *ripemd160 = EVP_MD_fetch(NULL, "ripemd160", NULL);
     if (ripemd160 == NULL)
     {
         unsigned long err = ERR_get_error();
         fprintf(stderr, "ripemd160 : %s\n", ERR_error_string(err, NULL));
         return 1;
     }
+#else
+    const EVP_MD *sha512 = EVP_sha512();
+    EVP_DigestInit_ex(ctx1, sha512, NULL);
+    const EVP_MD *ripemd160 = EVP_ripemd160();
+#endif
+    unsigned char hash[EVP_MAX_MD_SIZE];
     size_t c;
     for (x = 0; x < d2.quot; x += 16)
     {
