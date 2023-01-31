37e060bdfb452e763e68b2b0bac3f01456bc9d47
Teruteru <teruterubouzu1024@gmail.com>
2023-01-31 15:29:55 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -66,46 +66,12 @@
 #include <openssl/types.h>
 #endif
 
-#define IN                                                                    \
-    "MEwDAgcAAgEgAiEA7Vo1+"                                                   \
-    "Orf2xuuu6hTPAPldSfrUZZ7WYAzpRcO5DoYFLoCIF1JKVBctOGvMOy495O/"             \
-    "BWFuFEYH4i1f6vU0b9+a64RD"
-
-int gettimebaserandom(unsigned char *a, size_t b)
+struct color
 {
-    if (a == NULL || b == 0)
-    {
-        return 1;
-    }
-    struct timespec spec;
-    for (size_t i = 0; i < b; i++)
-    {
-        clock_gettime(CLOCK_REALTIME, &spec);
-        a[i] = spec.tv_nsec & 0xff;
-    }
-    return 0;
-}
-
-static volatile int running = 1;
-static pthread_barrier_t barrier;
-
-static void *func(void *arg)
-{
-    if (arg == NULL)
-    {
-        return NULL;
-    }
-    size_t *count = (size_t *)arg;
-    pthread_barrier_wait(&barrier);
-    register size_t d = 0;
-    while (running)
-    {
-        d++;
-    }
-    *count = d;
-    return arg;
-}
-
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
 /**
  * @brief
  * ↓2回連続getFloatで-1が出るseed 2つ
@@ -127,52 +93,16 @@ static void *func(void *arg)
  */
 int hiho(int argc, char **argv, const char *const *envp)
 {
-    size_t count = 0;
-    pthread_t thread = 0;
-    pthread_barrier_init(&barrier, NULL, 2);
-    struct timespec spec;
-    spec.tv_sec = 1;
-    spec.tv_nsec = 0;
-    pthread_create(&thread, NULL, func, &count);
-    pthread_barrier_wait(&barrier);
-    nanosleep(&spec, NULL);
-    running = 0;
-    pthread_join(thread, NULL);
-    pthread_barrier_destroy(&barrier);
-    printf("%zu\n", count);
-
-    GError *error = NULL;
-    gboolean ret = FALSE;
-
-    g_autoptr(GSocketClient) sclient = g_socket_client_new();
-    g_autoptr(GSocketConnection) connection = g_socket_client_connect_to_host(
-        sclient, "192.168.12.8:8442", 0, NULL, &error);
-    if (connection == NULL || error != NULL)
-    {
-        g_warning("g_socket_client_connect_to_host: %s", error->message);
-        g_error_free(error);
-    }
-    GVariantBuilder *builder = g_variant_builder_new(G_VARIANT_TYPE_ARRAY);
-    g_variant_builder_add(builder, "i", 111);
-    g_variant_builder_add(builder, "i", 222);
-    g_autoptr(GVariant) array = g_variant_builder_end(builder);
-    g_autoptr(GVariant) return_value = NULL;
-    g_autoptr(JsonrpcClient) client
-        = jsonrpc_client_new(G_IO_STREAM(connection));
-    ret = jsonrpc_client_call(client, "add", array, NULL, &return_value,
-                              &error);
-    if (return_value == NULL || ret != TRUE)
-    {
-        g_warning("jsonrpc_client_call: %s", error->message);
-        g_error_free(error);
-        return 1;
-    }
-    ret = jsonrpc_client_close(client, NULL, &error);
-    if (!ret)
-    {
-        g_warning("jsonrpc_client_close: %s", error->message);
-        g_error_free(error);
-        return 1;
-    }
+    unsigned char color[3] = { 0 };
+    getrandom(&color, 3, 0);
+    printf("%u\n", color[0]);
+    printf("%u\n", color[1]);
+    printf("%u\n", color[2]);
+    struct color color2 = { 0 };
+    getrandom(&color2, sizeof(struct color), 0);
+    printf("%02x%02x%02x\n", color2.red, color2.green, color2.blue);
+    printf("%u\n", color2.red);
+    printf("%u\n", color2.green);
+    printf("%u\n", color2.blue);
     return 0;
 }
