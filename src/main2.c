6ca71de86cc18d167ce6dcb578bab614fcd138a1
Teruteru <teruterubouzu1024@gmail.com>
2023-02-09 16:14:58 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -91,59 +91,137 @@
  */
 int hiho(int argc, char **argv, char *const *envp)
 {
-    char rpath[PATH_MAX] = "";
+#if 0
     char wpath[PATH_MAX] = "";
-    int rdfd = -1;
-    int wrfd = -1;
-    unsigned char *rdmap = NULL;
-    unsigned char *wrmap = mmap(NULL, 16777216 * 64, PROT_READ | PROT_WRITE,
-                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (wrmap == MAP_FAILED)
+    int prikeyfd = -1;
+    int pubkeyfd = -1;
+    int trimmedpubkeyfd = -1;
+    FILE *prikeyf = NULL;
+    FILE *pubkeyf = NULL;
+    FILE *trimmedpubkeyf = NULL;
+    EC_GROUP *secp256k1 = EC_GROUP_new_by_curve_name(NID_secp256k1);
+    BN_CTX *ctx = NULL;
+    BIGNUM *prikeybn = NULL;
+    EC_POINT *pubkeyp = NULL;
+    unsigned char *prikeymap = NULL;
+    unsigned char *pubkeymap
+        = mmap(NULL, 16777216 * 65, PROT_READ | PROT_WRITE,
+               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (pubkeymap == MAP_FAILED)
+    {
+        perror("mmap read");
+        return 1;
+    }
+    unsigned char *trimmedpubkeymap
+        = mmap(NULL, 16777216 * 64, PROT_READ | PROT_WRITE,
+               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (trimmedpubkeymap == MAP_FAILED)
     {
         perror("mmap write");
         return 1;
     }
     ssize_t d = 0;
-    for (size_t i = 0; i < 256; i++)
+    size_t j = 0;
+    for (size_t i = 8; i < 256; i++)
     {
-        snprintf(rpath, PATH_MAX, "/mnt/d/keys/public/publicKeys%zu.bin", i);
-        rdfd = open(rpath, O_RDONLY);
-        if (rdfd < 0)
+        // open private key file
+        snprintf(rpath, PATH_MAX, "/mnt/d/keys/private/privateKeys%zu.bin", i);
+        prikeyfd = open(rpath, O_RDONLY);
+        if (prikeyfd < 0)
         {
             perror("open read");
             return 1;
         }
-        rdmap = mmap(NULL, 16777216 * 65, PROT_READ, MAP_SHARED, rdfd, 0);
-        if (rdmap == MAP_FAILED)
+        // 秘密鍵ファイルをメモリーにマッピング
+        prikeymap
+            = mmap(NULL, 16777216 * 32, PROT_READ, MAP_PRIVATE, prikeyfd, 0);
+        if (prikeymap == MAP_FAILED)
         {
             perror("mmap read");
             return 1;
         }
-        close(rdfd);
-        snprintf(wpath, PATH_MAX,
-                 "/mnt/d/keys/public/trimmed/publicKeys%zu.bin", i);
-        wrfd = open(wpath, O_WRONLY | O_CREAT, 0700);
-        if (wrfd < 0)
+#pragma omp parallel default(none)                                            \
+    shared(prikeymap, secp256k1, trimmedpubkeymap,                            \
+           pubkeymap) private(ctx, j, prikeybn, pubkeyp)
         {
-            perror("open write");
+            ctx = BN_CTX_new();
+            BN_CTX_start(ctx);
+            prikeybn = BN_CTX_get(ctx);
+            pubkeyp = EC_POINT_new(secp256k1);
+#pragma omp for
+            for (j = 0; j < 16777216; j++)
+            {
+                BN_bin2bn(prikeymap + j * 32, 32, prikeybn);
+                EC_POINT_mul(secp256k1, pubkeyp, prikeybn, NULL, NULL, ctx);
+                EC_POINT_point2oct(secp256k1, pubkeyp,
+                                   POINT_CONVERSION_UNCOMPRESSED,
+                                   pubkeymap + 65 * j, 65, ctx);
+            }
+            BN_CTX_end(ctx);
+            BN_CTX_free(ctx);
+            EC_POINT_free(pubkeyp);
+        }
+        for (j = 0; j < 16777216; j++)
+        {
+            memcpy(trimmedpubkeymap + 64 * j, pubkeymap + 65 * j + 1, 64);
+        }
+        snprintf(rpath, PATH_MAX, "/mnt/d/keys/public/publicKeys%zu.bin", i);
+        pubkeyf = fopen(rpath, "wb");
+        if (pubkeyf == NULL)
+        {
+            perror("fopen pubkeyf");
             return 1;
         }
-        for (size_t j = 0; j < 16777216; j++)
-        {
-            memcpy(wrmap + 64 * i, rdmap + 65 * i + 1, 64);
-        }
-        munmap(rdmap, 1677716 * 65);
-        if (write(wrfd, wrmap, 16777216UL * 64) < 0)
+        if (fwrite(pubkeymap, 65, 16777216, pubkeyf) < 16777216)
         {
             perror("write");
         }
-        if (close(wrfd) == -1)
+        fclose(pubkeyf);
+        snprintf(wpath, PATH_MAX,
+                 "/mnt/d/keys/public/trimmed/publicKeys%zu.bin", i);
+        trimmedpubkeyf = fopen(wpath, "wb");
+        if (trimmedpubkeyf == NULL)
+        {
+            perror("fopen trimmedpubkeyf");
+            return 1;
+        }
+        if (fwrite(trimmedpubkeymap, 64, 16777216UL, trimmedpubkeyf)
+            < 16777216)
+        {
+            perror("write trimmedpubkeymap");
+        }
+        if (fclose(trimmedpubkeyf) == -1)
         {
             perror("close");
         }
         printf("%s done.\n", rpath);
     }
-    munmap(wrmap, 16777216 * 64);
-
+    munmap(pubkeymap, 16777216 * 65);
+    munmap(trimmedpubkeymap, 16777216 * 64);
+    EC_GROUP_free(secp256k1);
+#else
+    if (argc < 2)
+    {
+        return 1;
+    }
+    size_t a = 0x01ffffff;
+    size_t b = 536870912;
+    size_t needs = b - a;
+    char rpath[PATH_MAX] = "";
+    strncpy(rpath, argv[1], PATH_MAX);
+    int fd = open(rpath, O_RDONLY);
+    unsigned char *m
+        = mmap(NULL, 16777216UL * 32, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    unsigned char mam[32];
+    memset(mam, 0, 32);
+    size_t l = argc >= 3 ? strtoul(argv[2], NULL, 0): 32;
+    unsigned char *m2 = memmem(m, 16777216UL * 32, mam, l);
+    if (m2 != NULL)
+    {
+        printf("0x%016lx, %ld\n", m2 - m, m2 - m);
+    }
+    munmap(m, 16777216 * 32);
+#endif
     return 0;
 }
