d953ea17136bdf4e0d95cd53da471555d176dd16
Teruteru <teruterubouzu1024@gmail.com>
2023-04-12 15:11:51 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -75,52 +75,6 @@
 #include <openssl/types.h>
 #endif
 
-#define LOWER_THRESHOLD 1.0e-6
-#define UPPER_BOUND 1.0e+4
-#define INF 300
-
-double complex zeta(double complex s)
-{
-    double complex outer_sum = 0;
-    double complex inner_sum = 0;
-    size_t j = 0;
-    double complex c1 = 0;
-    double complex c2 = 0;
-    double complex c3 = 0;
-    double complex prev = 1000000000;
-    for (size_t m = 0; m <= INF; m++)
-    {
-        inner_sum = 0;
-        for (j = 1; j <= m; j++)
-        {
-            c1 = ((j - 1) % 2 == 0) ? 1 : (-1);
-            c2 = binomial(m - 1, j - 1);
-            c3 = cpow(j, -s);
-            inner_sum += c1 * c2 * c3;
-        }
-        // FIXME cpow(2, -m)が小さすぎて0になってしまうためinfになる
-        inner_sum = inner_sum * cpow(2, -m) / (1 - cpow(2, 1 - s));
-
-        outer_sum += inner_sum;
-
-        if (cabs(prev - inner_sum) < LOWER_THRESHOLD)
-        {
-            break;
-        }
-        if (cabs(outer_sum) > UPPER_BOUND)
-        {
-            break;
-        }
-        prev = inner_sum;
-    }
-
-    return outer_sum;
-}
-#define WIDTH 1920
-#define HEIGHT 1080
-
-static size_t wordIndex(size_t bitIndex) { return bitIndex >> 3; }
-
 /**
  * @brief
  * ↓2回連続getFloatで-1が出るseed 2つ
@@ -182,7 +136,9 @@ int entrypoint(int argc, char **argv, char *const *envp)
     char passphrase[UUID_STR_LEN] = "";
     long signingKeyNonce = 0;
     long encryptionKeyNonce = 1;
-    unsigned char nonce[2] = { 0x00, 0x01 };
+    size_t counter = 0;
+    unsigned char nonce[9] = { 0x00 };
+    size_t nonce_length = 0;
     unsigned char *nonce1 = nonce + 1;
     unsigned long tmp = 0;
     int nice = 20;
@@ -197,71 +153,135 @@ int entrypoint(int argc, char **argv, char *const *envp)
     BIGNUM *prikey = BN_CTX_get(bnctx);
     const EVP_MD *sha512 = EVP_sha512();
     const EVP_MD *ripemd160 = EVP_ripemd160();
+    EVP_MD_CTX *sha512ctx0 = EVP_MD_CTX_new();
     EVP_MD_CTX *sha512ctx1 = EVP_MD_CTX_new();
     EVP_MD_CTX *sha512ctx2 = EVP_MD_CTX_new();
     EVP_MD_CTX *ripemd160ctx = EVP_MD_CTX_new();
     unsigned char hash[EVP_MAX_MD_SIZE];
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
     EVP_DigestInit_ex2(sha512ctx2, sha512, NULL);
 #else
     EVP_DigestInit_ex(sha512ctx1, sha512, NULL);
+    EVP_DigestInit_ex(sha512ctx2, sha512, NULL);
 #endif
     do
     {
         // passphrase generate
         uuid_generate_random(uuid);
         uuid_unparse_lower(uuid, passphrase);
-
+        nice = 20;
+        counter = 0;
         // private keys generate
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-        EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
+        EVP_DigestInit_ex2(sha512ctx0, sha512, NULL);
 #else
-        EVP_DigestInit_ex(sha512ctx1, sha512, NULL);
+        EVP_DigestInit_ex(sha512ctx0, sha512, NULL);
 #endif
-        EVP_DigestUpdate(sha512ctx1, passphrase, 36);
-        EVP_MD_CTX_copy_ex(sha512ctx2, sha512ctx1);
+        EVP_DigestUpdate(sha512ctx0, passphrase, 36);
+        do
+        {
+            // TODO 公開鍵導出を並列化(parallel+sections)
+            EVP_MD_CTX_copy_ex(sha512ctx1, sha512ctx0);
+            EVP_MD_CTX_copy_ex(sha512ctx2, sha512ctx0);
 
-        EVP_DigestUpdate(sha512ctx1, nonce, 1);
-        EVP_DigestFinal_ex(sha512ctx1, potentialPrivSigningKey, NULL);
+            if (counter < 253)
+            {
+                nonce[0] = (unsigned char)counter;
+                nonce_length = 1;
+            }
+            else if (counter < 65536)
+            {
+                nonce[0] = 0xfd;
+                *((uint16_t *)nonce1) = htobe16((uint16_t)counter);
+                nonce_length = 3;
+            }
+            else if (counter < 4294967296L)
+            {
+                nonce[0] = 0xfe;
+                *((uint32_t *)nonce1) = htobe16((uint32_t)counter);
+                nonce_length = 5;
+            }
+            else if (counter <= 18446744073709551615UL)
+            {
+                nonce[0] = 0xff;
+                *((uint64_t *)nonce1) = htobe16((uint64_t)counter);
+                nonce_length = 9;
+            }
 
-        EVP_DigestUpdate(sha512ctx2, nonce1, 1);
-        EVP_DigestFinal_ex(sha512ctx2, potentialPrivEncryptionKey, NULL);
+            EVP_DigestUpdate(sha512ctx1, nonce, nonce_length);
+            EVP_DigestFinal_ex(sha512ctx1, potentialPrivSigningKey, NULL);
 
-        // public keys generate
-        BN_bin2bn(potentialPrivSigningKey, 32, prikey);
-        EC_POINT_mul(secp256k1, pubkey, prikey, NULL, NULL, bnctx);
-        EC_POINT_point2oct(secp256k1, pubkey, POINT_CONVERSION_UNCOMPRESSED,
-                           potentialPubSigningKey, 65, bnctx);
+            counter++;
+            if (counter < 253)
+            {
+                nonce[0] = (unsigned char)counter;
+                nonce_length = 1;
+            }
+            else if (counter < 65536)
+            {
+                nonce[0] = 0xfd;
+                *((uint16_t *)nonce1) = htobe16((uint16_t)counter);
+                nonce_length = 3;
+            }
+            else if (counter < 4294967296L)
+            {
+                nonce[0] = 0xfe;
+                *((uint32_t *)nonce1) = htobe16((uint32_t)counter);
+                nonce_length = 5;
+            }
+            else if (counter <= 18446744073709551615UL)
+            {
+                nonce[0] = 0xff;
+                *((uint64_t *)nonce1) = htobe16((uint64_t)counter);
+                nonce_length = 9;
+            }
 
-        BN_bin2bn(potentialPrivEncryptionKey, 32, prikey);
-        EC_POINT_mul(secp256k1, pubkey, prikey, NULL, NULL, bnctx);
-        EC_POINT_point2oct(secp256k1, pubkey, POINT_CONVERSION_UNCOMPRESSED,
-                           potentialPubEncryptionKey, 65, bnctx);
+            EVP_DigestUpdate(sha512ctx2, nonce, nonce_length);
+            EVP_DigestFinal_ex(sha512ctx2, potentialPrivEncryptionKey, NULL);
+
+            // public keys generate
+            BN_bin2bn(potentialPrivSigningKey, 32, prikey);
+            EC_POINT_mul(secp256k1, pubkey, prikey, NULL, NULL, bnctx);
+            EC_POINT_point2oct(secp256k1, pubkey,
+                               POINT_CONVERSION_UNCOMPRESSED,
+                               potentialPubSigningKey, 65, bnctx);
+
+            BN_bin2bn(potentialPrivEncryptionKey, 32, prikey);
+            EC_POINT_mul(secp256k1, pubkey, prikey, NULL, NULL, bnctx);
+            EC_POINT_point2oct(secp256k1, pubkey,
+                               POINT_CONVERSION_UNCOMPRESSED,
+                               potentialPubEncryptionKey, 65, bnctx);
 
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-        EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
+            EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
 #else
-        EVP_DigestInit_ex(sha512startctx1, sha512, NULL);
+            EVP_DigestInit_ex(sha512startctx1, sha512, NULL);
 #endif
-        // ripe generate
-        EVP_DigestUpdate(sha512ctx1, potentialPubSigningKey, 65);
-        EVP_DigestUpdate(sha512ctx1, potentialPubEncryptionKey, 65);
-        EVP_DigestFinal(sha512ctx1, hash, NULL);
+            // ripe generate
+            EVP_DigestUpdate(sha512ctx1, potentialPubSigningKey, 65);
+            EVP_DigestUpdate(sha512ctx1, potentialPubEncryptionKey, 65);
+            EVP_DigestFinal(sha512ctx1, hash, NULL);
 
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-        EVP_DigestInit_ex2(ripemd160ctx, ripemd160, NULL);
+            EVP_DigestInit_ex2(ripemd160ctx, ripemd160, NULL);
 #else
-        EVP_DigestInit_ex(ripemd160ctx, ripemd160, NULL);
+            EVP_DigestInit_ex(ripemd160ctx, ripemd160, NULL);
 #endif
-        EVP_DigestUpdate(ripemd160ctx, hash, 64);
-        EVP_DigestFinal(ripemd160ctx, hash, NULL);
+            EVP_DigestUpdate(ripemd160ctx, hash, 64);
+            EVP_DigestFinal(ripemd160ctx, hash, NULL);
 
-        // check
-        tmp = be64toh(*(unsigned long *)hash);
-        nice = tmp == 0UL ? 64 : __builtin_clzl(tmp);
+            // check
+            tmp = be64toh(*(unsigned long *)hash);
+            nice = tmp == 0UL ? 64 : __builtin_clzl(tmp);
+            counter++;
+        } while (nice < 8);
     } while (nice < 24);
     printf("%s, nice = %d\n", passphrase, nice);
+    EVP_MD_CTX_free(sha512ctx0);
+    EVP_MD_CTX_free(sha512ctx1);
     EVP_MD_CTX_free(sha512ctx2);
+    EVP_MD_CTX_free(ripemd160ctx);
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     OSSL_PROVIDER_unload(def);
     OSSL_PROVIDER_unload(legacy);
