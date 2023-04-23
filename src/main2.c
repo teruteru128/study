12c5ef2c0056a2ed452328dac833a5853efc64a5
Teruteru <teruterubouzu1024@gmail.com>
2023-04-23 15:31:21 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -106,9 +106,10 @@
  */
 int entrypoint(int argc, char **argv, char *const *envp)
 {
-    if (argc < 2)
+    size_t snum = 0;
+    if (argc >= 2)
     {
-        return 1;
+        snum = strtoul(argv[1], NULL, 10);
     }
     size_t y;
     size_t yy;
@@ -116,6 +117,7 @@ int entrypoint(int argc, char **argv, char *const *envp)
     size_t x;
     size_t xx;
     size_t xxx;
+    size_t ii;
     unsigned char *key = malloc(65 * (1UL << 24));
     FILE *fd = fopen("/mnt/d/keys/public/publicKeys0.bin", "rb");
     if (fd == NULL)
@@ -137,82 +139,80 @@ int entrypoint(int argc, char **argv, char *const *envp)
 #endif
     const EVP_MD *sha512 = EVP_sha512();
     const EVP_MD *ripemd160 = EVP_ripemd160();
-    for (y = 0; y < 256; y++)
+    time_t start = 0;
+    for (size_t i = snum; i < 65536; i++)
     {
-        for (x = 0; x < 256; x++)
-        {
+        y = (i >> 8) & 0xff;
+        x = (i >> 0) & 0xff;
+        start = time(NULL);
 #pragma omp parallel default(none) private(xx, yy, xxx, yyy)                  \
     shared(sha512, ripemd160, x, y, key)
-            {
-                EVP_MD_CTX *sha512ctx1 = EVP_MD_CTX_new();
-                EVP_MD_CTX *sha512ctx2 = EVP_MD_CTX_new();
-                EVP_MD_CTX *ripemd160ctx1 = EVP_MD_CTX_new();
+        {
+            EVP_MD_CTX *sha512ctx1 = EVP_MD_CTX_new();
+            EVP_MD_CTX *sha512ctx2 = EVP_MD_CTX_new();
+            EVP_MD_CTX *ripemd160ctx1 = EVP_MD_CTX_new();
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-                EVP_DigestInit_ex2(sha512ctx2, sha512, NULL);
+            EVP_DigestInit_ex2(sha512ctx2, sha512, NULL);
 #else
-                EVP_DigestInit_ex(sha512ctx2, sha512, NULL);
+            EVP_DigestInit_ex(sha512ctx2, sha512, NULL);
 #endif
-                unsigned char hash[EVP_MAX_MD_SIZE];
-                uint64_t leading64 = 0;
-                size_t offset_x = 0;
-                size_t offset_y = 0;
+            unsigned char hash[EVP_MAX_MD_SIZE];
+            uint64_t leading64 = 0;
+            size_t offset_x = 0;
+            size_t offset_y = 0;
 #pragma omp for
-                for (yy = 0; yy < 256; yy++)
+            for (yy = 0; yy < 256; yy++)
+            {
+                for (xx = 0; xx < 256; xx++)
                 {
-                    for (xx = 0; xx < 256; xx++)
+                    offset_y = (y << 16) + (yy << 8);
+                    for (yyy = 0; yyy < 256; yyy++, offset_y++)
                     {
-                        offset_y = (y << 16) + (yy << 8);
-                        for (yyy = 0; yyy < 256; yyy++, offset_y++)
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+                        EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
+#else
+                        EVP_DigestInit_ex(sha512ctx1, sha512, NULL);
+#endif
+                        EVP_DigestUpdate(sha512ctx1,
+                                         key + (offset_y << 6) + offset_y, 65);
+                        offset_x = (x << 16) + (xx << 8);
+                        for (xxx = 0; xxx < 256; xxx++, offset_x++)
                         {
-#if OPENSSL_VERSION_NUMBER >= 0x30000000L
-                            EVP_DigestInit_ex2(sha512ctx1, sha512, NULL);
-#else
-                            EVP_DigestInit_ex(sha512ctx1, sha512, NULL);
-#endif
+                            EVP_MD_CTX_copy_ex(sha512ctx2, sha512ctx1);
                             EVP_DigestUpdate(sha512ctx1,
-                                             key + (offset_y << 6) + offset_y,
+                                             key + (offset_x << 6) + offset_x,
                                              65);
-                            offset_x = (x << 16) + (xx << 8);
-                            for (xxx = 0; xxx < 256; xxx++, offset_x++)
-                            {
-                                EVP_MD_CTX_copy_ex(sha512ctx2, sha512ctx1);
-                                EVP_DigestUpdate(
-                                    sha512ctx1,
-                                    key + (offset_x << 6) + offset_x, 65);
-                                EVP_DigestFinal_ex(sha512ctx2, hash, NULL);
+                            EVP_DigestFinal_ex(sha512ctx2, hash, NULL);
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-                                EVP_DigestInit_ex2(ripemd160ctx1, ripemd160,
-                                                   NULL);
+                            EVP_DigestInit_ex2(ripemd160ctx1, ripemd160, NULL);
 #else
-                                EVP_DigestInit_ex(ripemd160ctx1, ripemd160,
-                                                  NULL);
+                            EVP_DigestInit_ex(ripemd160ctx1, ripemd160, NULL);
 #endif
-                                EVP_DigestUpdate(ripemd160ctx1, hash, 64);
-                                EVP_DigestFinal_ex(ripemd160ctx1, hash, NULL);
-                                leading64 = htobe64(*(unsigned long *)hash);
-                                if (leading64 & 0xffffffffff000000UL)
-                                {
-                                    continue;
-                                }
+                            EVP_DigestUpdate(ripemd160ctx1, hash, 64);
+                            EVP_DigestFinal_ex(ripemd160ctx1, hash, NULL);
+                            leading64 = htobe64(*(unsigned long *)hash);
+                            if (leading64 & 0xffffffffff000000UL)
+                            {
+                                continue;
+                            }
 #pragma omp critical
-                                {
-                                    size_t count
-                                        = (leading64 == 0)
-                                              ? 64
-                                              : __builtin_clzl(leading64);
-                                    printf("%zu: %zu, %zu\n", count, offset_y,
-                                           offset_x);
-                                }
+                            {
+                                size_t count = (leading64 == 0)
+                                                   ? 64
+                                                   : __builtin_clzl(leading64);
+                                printf("%zu: %zu, %zu\n", count, offset_y,
+                                       offset_x);
                             }
                         }
                     }
                 }
-                EVP_MD_CTX_free(sha512ctx1);
-                EVP_MD_CTX_free(sha512ctx2);
-                EVP_MD_CTX_free(ripemd160ctx1);
             }
-            fprintf(stderr, "%zu/65536 done\n", x + 1);
+            EVP_MD_CTX_free(sha512ctx1);
+            EVP_MD_CTX_free(sha512ctx2);
+            EVP_MD_CTX_free(ripemd160ctx1);
         }
+        fprintf(stderr, "%zu/65536 done, %ldseconds\n", x + 1,
+                time(NULL) - start);
     }
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     OSSL_PROVIDER_unload(def);
