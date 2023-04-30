8f05753a04528636bdabcfba7fe8caae6209709b
Teruteru <teruterubouzu1024@gmail.com>
2023-04-30 13:28:12 +0900

    Update

--- src/main2.c
+++ src/main2.c
@@ -79,6 +79,30 @@
 
 /**
  * @brief
+ *
+ * @param key
+ * @param index
+ * @return int onerror: 0, onsuccess: 1
+ */
+int load(unsigned char *key, size_t index)
+{
+    size_t filenumber = (index >> 24) & 0xffUL;
+    size_t fileoffset = ((index >> 0) & 0xffffffUL) * 65;
+    char in[PATH_MAX] = "";
+    snprintf(in, PATH_MAX, "/mnt/d/keys/public/publicKeys%zu.bin", filenumber);
+    FILE *fin = fopen(in, "rb");
+    if (fin == NULL)
+    {
+        return 0;
+    }
+    fseek(fin, SEEK_SET, fileoffset);
+    size_t num = fread(key, 65, 1, fin);
+    fclose(fin);
+    return num == 1;
+}
+
+/**
+ * @brief
  * ↓2回連続getFloatで-1が出るseed 2つ
  * 125352706827826
  * 116229385253865
@@ -110,9 +134,48 @@ int entrypoint(int argc, char **argv, char *const *envp)
     {
         return 1;
     }
-    for (size_t i = 0; envp[i] != NULL; i++)
+    uint64_t number = 0;
+    size_t a = 0;
+    uint64_t x = 0;
+    uint64_t y = 0;
+    unsigned char sigkey[65];
+    unsigned char enckey[65];
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    OSSL_PROVIDER *legacy = OSSL_PROVIDER_load(NULL, "legacy");
+    OSSL_PROVIDER *def = OSSL_PROVIDER_load(NULL, "default");
+#endif
+    const EVP_MD *sha512 = EVP_sha512();
+    const EVP_MD *ripemd160 = EVP_ripemd160();
+    EVP_MD_CTX *md = EVP_MD_CTX_new();
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    do
     {
-        printf("%s\n", envp[i]);
-    }
+        a = getrandom(&number, 8, 0);
+        x = (number >> 32) & 0xffffffffUL;
+        y = (number >> 0) & 0xffffffffUL;
+        load(sigkey, x);
+        load(enckey, y);
+        EVP_DigestInit_ex2(md, sha512, NULL);
+        EVP_DigestUpdate(md, sigkey, 65);
+        EVP_DigestUpdate(md, enckey, 65);
+        EVP_DigestFinal_ex(md, hash, NULL);
+        EVP_DigestInit_ex2(md, ripemd160, NULL);
+        EVP_DigestUpdate(md, hash, 64);
+        EVP_DigestFinal_ex(md, hash, NULL);
+        if (hash[0] == 0)
+        {
+            for (size_t i = 0; i < 20; i++)
+            {
+                printf("%02x", hash[i]);
+            }
+            printf("\n");
+            printf("%" PRIu64 ", %" PRIu64 ", %" PRIu64 "\n", number, x, y);
+        }
+    } while (hash[0] != 0);
+    EVP_MD_CTX_free(md);
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    OSSL_PROVIDER_unload(def);
+    OSSL_PROVIDER_unload(legacy);
+#endif
     return 0;
 }
