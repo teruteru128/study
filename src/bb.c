5d4ddd3294199bd959df62ae8baa33dd6acabeb6
Teruteru <teruterubouzu1024@gmail.com>
2025-12-22 23:30:34 +0900

    Big Brother is watching you.

---
+++ src/bb.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 86400; i++)
+    {
+        printf("甘受せよさあ思慮は今罪と知るべし\n");
+        sleep(1);
+    }
+    return EXIT_SUCCESS;
+}
