f5c943d3ae1feebefa3a06f3bf9bc150d590b6b7
Teruteru <teruterubouzu1024@gmail.com>
2025-12-28 23:55:57 +0900

    Update

--- src/openmpsample5.c
+++ src/openmpsample5.c
@@ -10,11 +10,16 @@ int main(int argc, char *argv[])
     fprintf(stdout, "スレッド情報 omp_get_num_threads=%d\n", omp_get_num_threads());
     fprintf(stdout, "スレッド情報 omp_get_thread_num=%d\n", omp_get_thread_num());
     size_t i;
-#pragma omp parallel for
-    for (i = 0; i < 1024; ++i)
+    fprintf(stdout, "--\n");
+#pragma omp parallel
     {
-        // ここが並列に処理されます。並列数は、コア数分です。
-        fprintf(stdout, "hello world: %lu, %d\n", i, omp_get_thread_num());
+        fprintf(stdout, "スレッド情報 omp_get_num_threads=%d, omp_get_thread_num=%d\n", omp_get_num_threads(), omp_get_thread_num());
+#pragma omp for
+        for (i = 0; i < 8192; ++i)
+        {
+            // ここが並列に処理されます。並列数は、コア数分です。
+            fprintf(stdout, "hello world: %lu, %d\n", i, omp_get_thread_num());
+        }
     }
     return EXIT_SUCCESS;
 }
--- src/totp.c
+++ src/totp.c
@@ -1,4 +1,5 @@
 
+#define _DEFAULT_SOURCE
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -14,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <endian.h>
 
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
 #include <openssl/core_names.h>
@@ -21,30 +23,29 @@
 #include <openssl/types.h>
 #endif
 
-int hmac(const char *crypto, unsigned char *key, size_t keysiz,
-         unsigned char *msg, size_t msglen, unsigned char *hash, size_t *s)
+int hmac(const char *crypto, const unsigned char *key, const size_t keysiz,
+         const unsigned char *msg, const size_t msglen, unsigned char *hash, size_t *s)
 {
     // TODO: EVP_MAC が OpenSSL に直接導入されるのは ver. 3.0 以降
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
     EVP_MAC *mac = EVP_MAC_fetch(NULL, "HMAC", "provider=default");
     EVP_MAC_CTX *macctx = EVP_MAC_CTX_new(mac);
     OSSL_PARAM_BLD *param_bld = OSSL_PARAM_BLD_new();
-    OSSL_PARAM_BLD_push_utf8_string(param_bld, OSSL_MAC_PARAM_DIGEST, crypto,
-                                    0);
+    OSSL_PARAM_BLD_push_utf8_string(param_bld, OSSL_MAC_PARAM_DIGEST, crypto, 0);
     OSSL_PARAM *params = OSSL_PARAM_BLD_to_param(param_bld);
+    OSSL_PARAM_BLD_free(param_bld);
     EVP_MAC_init(macctx, key, keysiz, params);
     EVP_MAC_update(macctx, msg, msglen);
     EVP_MAC_final(macctx, hash, s, 20);
     EVP_MAC_CTX_free(macctx);
     OSSL_PARAM_free(params);
-    OSSL_PARAM_BLD_free(param_bld);
+    EVP_MAC_free(mac);
 #else
     // OpenSSL_add_all_algorithms();
     // OpenSSL_add_all_ciphers();
     // OpenSSL_add_all_digests();
     const EVP_MD *md = EVP_get_digestbyname(crypto);
-    EVP_PKEY *pkey
-        = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, NULL, key, (int)keysiz);
+    EVP_PKEY *pkey = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, NULL, key, (int)keysiz);
     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
     EVP_DigestSignInit(mdctx, NULL, md, NULL, pkey);
     EVP_DigestSignUpdate(mdctx, msg, msglen);
@@ -55,13 +56,15 @@ int hmac(const char *crypto, unsigned char *key, size_t keysiz,
     return 0;
 }
 
-static const int32_t DIGITS_POWER[9]
-    //  0  1   2    3     4      5       6        7         8
-    = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
+static const int32_t DIGITS_POWER[10]
+    // 0  1   2    3     4      5       6        7         8          9
+    = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
 
-char *generateTOTP(unsigned char *key, size_t keysiz, time_t time,
-                   int returnDigits, const char *crypto)
+void generateTOTP(const unsigned char *key, const size_t keysiz, const time_t time,
+                  int returnDigits, const char *crypto, char *out_str)
 {
+    if (returnDigits < 1 || returnDigits > 10)
+        return; // 境界チェック
     uint64_t msgword = (uint64_t)time;
     unsigned char *msg = (unsigned char *)&msgword;
 
@@ -69,27 +72,19 @@ char *generateTOTP(unsigned char *key, size_t keysiz, time_t time,
     size_t hashsiz = EVP_MAX_MD_SIZE;
     if (hmac(crypto, key, keysiz, msg, 8, hash, &hashsiz) != 0)
     {
-        return NULL;
+        return;
     }
 
     int offset = hash[hashsiz - 1] & 0x0f;
 
-    int binary
-        = ((hash[offset] & 0x7f) << 24) | ((hash[offset + 1] & 0xff) << 16)
-          | ((hash[offset + 2] & 0xff) << 8) | (hash[offset + 3] & 0xff);
+    uint32_t val;
+    memcpy(&val, hash + offset, 4);
+    int32_t binary = be32toh(val) & 0x7fffffff;
 
-    int otp = binary % DIGITS_POWER[returnDigits];
+    int32_t otp = binary % DIGITS_POWER[returnDigits];
 
-    // テキスト整形
-    char format[16] = "";
-    snprintf(format, 16, "%%0%dd", returnDigits);
-    char *result = malloc(returnDigits + 1);
-    // 念のためゼロクリア
-    memset(result, 0, returnDigits + 1);
     // OTP文字列生成
-    snprintf(result, returnDigits + 1, format, otp);
-
-    return result;
+    snprintf(out_str, returnDigits + 1, "%0*d", returnDigits, (int)otp);
 }
 
 /**
@@ -100,9 +95,9 @@ char *generateTOTP(unsigned char *key, size_t keysiz, time_t time,
  * @param returnDigits
  * @return char*
  */
-char *generateTOTP_SHA1(const char *key, time_t time, size_t returnDigits)
+void generateTOTP_SHA1(const char *key, time_t time, size_t returnDigits, char *out_str)
 {
-    return generateTOTP(key, strlen(key), time, returnDigits, "SHA-1");
+    generateTOTP(key, strlen(key), time, returnDigits, "SHA-1", out_str);
 }
 
 int main(int argc, char *argv[])
@@ -111,9 +106,12 @@ int main(int argc, char *argv[])
     const time_t T0 = 0;
     time_t unixtime = time(NULL);
     time_t T = (unixtime - T0) / X;
+    char totp[11];
     char key[] = "3132333435363738393031323334353637383930";
-    char *totp = generateTOTP_SHA1(key, T, 6);
-    printf("%s\n", totp);
-    free(totp);
+    for (int i = 6; i <= 9; i++)
+    {
+        generateTOTP_SHA1(key, T, i, totp);
+        printf("%s\n", totp);
+    }
     return EXIT_SUCCESS;
 }
