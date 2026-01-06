e4749903b0e47a02956826bdb1e62a79bdb2ec56
Teruteru <teruterubouzu1024@gmail.com>
2026-01-06 23:34:47 +0900

    Update

--- src/114514.c
+++ src/114514.c
@@ -8,38 +8,46 @@
 
 static size_t getlength(MP_INT *n)
 {
-    size_t length;
-    char *n_str = malloc(mpz_sizeinbase(n, 10) + 2);
+    char n_str[229038];
     mpz_get_str(n_str, 10, n);
-    length = strlen(n_str);
-    free(n_str);
-    return length;
+    return strlen(n_str);
 }
 
+/**
+ * 114514桁の素数を探索するための事前ツール
+ */
 int main(int argc, char const *argv[])
 {
     MP_INT n;
+    char n_str[229038];
     mpz_init(&n);
     // 指数
-    int sisuu = 380404;
+    int sisuu;
     // 仮数
-    for (int kasuu = 3; kasuu <= 1000; kasuu++)
+    for (int kasuu = 1; kasuu <= 1000; kasuu++)
     {
+        sisuu = 380408;
         mpz_set_ui(&n, 2);
         mpz_mul_2exp(&n, &n, sisuu);
         mpz_mul_ui(&n, &n, kasuu);
-        mpz_add_ui(&n, &n, 10);
-        size_t length = getlength(&n);
-        while (length > MAX_LENGTH)
+        mpz_add_ui(&n, &n, 1);
+        mpz_get_str(n_str, 10, &n);
+        size_t length = strlen(n_str);
+        while (length >= MAX_LENGTH)
         {
+            if (length == MAX_LENGTH)
+            {
+                printf("2^%d*%d+1: %zu digits\n", sisuu, kasuu, length);
+            }
             sisuu--;
             mpz_set_ui(&n, 2);
             mpz_mul_2exp(&n, &n, sisuu);
             mpz_mul_ui(&n, &n, kasuu);
-            mpz_add_ui(&n, &n, 10);
-            length = getlength(&n);
+            mpz_add_ui(&n, &n, 1);
+            mpz_get_str(n_str, 10, &n);
+            length = strlen(n_str);
         }
-        printf("2^%d*%d+1: %zu digits\n", sisuu, kasuu, length);
+        fprintf(stderr, "%d done\n", kasuu);
     }
     mpz_clear(&n);
     return 0;
